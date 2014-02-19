c42dcd2b78a0e25f60d419b1b372268dca00aadf
Sameera Jayasoma <sameera.madushan@gmail.com>
2014-02-19 13:05:46 +0530

    Adding the carbon.kernel module. This will introduce the configuration and context model of the platform

---
+++ modules/kernel/src/main/java/org/wso2/carbon/kernel/internal/KernelActivator.java
@@ -0,0 +1,14 @@
+package org.wso2.carbon.kernel.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class KernelActivator implements BundleActivator {
+
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+    }
+}
