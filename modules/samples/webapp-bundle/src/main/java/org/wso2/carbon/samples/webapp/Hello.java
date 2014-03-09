93f689550c129af194b2772fc6caac42e02ca1ab
Kishanthan <kshanth2101@gmail.com>
2014-03-09 22:38:55 +0530

    adding a sample bundle for jetty integration testing purpose. this bundle can be deployed as a webapp

---
+++ modules/samples/webapp-bundle/src/main/java/org/wso2/carbon/samples/webapp/Hello.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.samples.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Hello extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doGet(req, res);
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        PrintWriter pw = res.getWriter();
+        pw.println("<html>");
+        pw.println("<head><title>Hello World Servlet</title></title>");
+        pw.println("<body>");
+        pw.println("<h1>Hello World Servlet</h1>");
+        pw.println("</body></html>");
+    }
+}
