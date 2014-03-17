40a76aa283e7d5b1cd848b0e49b26bc1a1224b7b
darshanasbg <darshana@wso2.com>
2014-03-17 22:05:45 +0530

    Adding login conetext api

---
+++ modules/identity/api/src/main/java/org/wso2/carbon/identity/carbon/CarbonRole.java
@@ -0,0 +1,91 @@
+ /*
+ *  Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.identity.carbon;
+
+import java.util.List;
+
+import org.wso2.carbon.identity.carbon.commons.EntityTree;
+import org.wso2.carbon.identity.carbon.commons.IdentityException;
+import org.wso2.carbon.identity.carbon.identifiers.EntryIdentifier;
+import org.wso2.carbon.identity.carbon.identifiers.RoleIdentifier;
+import org.wso2.carbon.identity.carbon.identifiers.StoreIdentifier;
+
+public interface CarbonRole<U extends CarbonUser, G extends CarbonGroup>
+		extends Role {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public EntryIdentifier getRoleEntryId();
+
+	/**
+	 * 
+	 * @return
+	 * @throws IdentityException
+	 */
+	public List<CarbonPermission> getPermissions() throws IdentityException;
+
+	/**
+	 * 
+	 * @return
+	 * @throws IdentityException
+	 */
+	public List<G> getGroups() throws IdentityException;
+
+	/**
+	 * 
+	 * @return
+	 * @throws IdentityException
+	 */
+	public List<U> getUsers() throws IdentityException;
+
+	/**
+	 * 
+	 * @return
+	 * @throws IdentityException
+	 */
+	public List<EntityTree> getChildren() throws IdentityException;
+
+	/**
+	 * 
+	 * @param childRoleIdentifier
+	 * @return
+	 * @throws IdentityException
+	 */
+	public boolean hasChild(RoleIdentifier childRoleIdentifier)
+			throws IdentityException;
+
+	/**
+	 * 
+	 * @param parentRoleIdentifier
+	 * @return
+	 * @throws IdentityException
+	 */
+	public boolean hasParent(RoleIdentifier parentRoleIdentifier)
+			throws IdentityException;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public StoreIdentifier getStoreIdentifier();
+
+}
