a08426b613991b1ad39e69a660562b2e9f6cb8ab
Shameera <shameera@apache.org>
2014-04-21 18:16:42 +0530

    Added a new module 'jaxrs-framework' to repository

---
+++ modules/jaxrs-framework/src/main/java/org/wso2/carbon/jaxrs/internal/JAXRSFrameworkBundleActivator.java
@@ -0,0 +1,37 @@
+/*
+*  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.jaxrs.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JAXRSFrameworkBundleActivator implements BundleActivator {
+    private static Logger logger = LoggerFactory.getLogger(JAXRSFrameworkBundleActivator.class);
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        logger.info("Activating JAXRS Framework bundle");
+
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        logger.info("Stopping JAXRS Framework bundle");
+    }
+}
