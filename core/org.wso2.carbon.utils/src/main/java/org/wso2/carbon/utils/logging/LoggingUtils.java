459caf6ba92611c964e28e5a40fb40ba728064f1
Gayashan Amarasinghe <chozrc@gmail.com>
2014-07-16 20:11:43 +0530

    getTenantAwareLogEvent method added to LoggingUtils which returns a TenantAwareLoggingEvent that wraps the passed LoggingEvent with tenant specific tenantId and serviceName

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/logging/LoggingUtils.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/logging/LoggingUtils.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
 
 import java.util.logging.LogRecord;
 
@@ -13,6 +14,36 @@ public class LoggingUtils {
     public static LoggingEvent getLogEvent(LogRecord record) {
         Priority level = getLogLevel(record.getLevel());
         return new LoggingEvent(record.getSourceClassName(), Logger.getLogger(record.getSourceClassName()), level, record.getMessage(), record.getThrown());
+    }
+
+    /**
+     * Returns a TenantAwareLoggingEvent that wraps the LoggingEvent with tenant specific
+     * tenantId and serviceName
+     *
+     * @param loggingEvent -  The LoggingEvent with the log content
+     * @param tenantId     - tenant Id of the tenant which triggered log event
+     * @param serviceName  - service name of the current log event
+     * @return a TenantAwareLoggingEvent
+     */
+    public static TenantAwareLoggingEvent getTenantAwareLogEvent(LoggingEvent loggingEvent,
+                                                                 int tenantId, String serviceName) {
+        Logger logger = Logger.getLogger(loggingEvent.getLoggerName());
+        TenantAwareLoggingEvent tenantAwareLoggingEvent;
+        ThrowableInformation throwableInformation = loggingEvent.getThrowableInformation();
+
+        Throwable throwable;
+        if (null == throwableInformation) { // null check
+            throwable = null;
+        } else {
+            throwable = throwableInformation.getThrowable();
+        }
+        tenantAwareLoggingEvent = new TenantAwareLoggingEvent(loggingEvent.fqnOfCategoryClass,
+                                                              logger, loggingEvent.timeStamp,
+                                                              loggingEvent.getLevel(),
+                                                              loggingEvent.getMessage(), throwable);
+        tenantAwareLoggingEvent.setTenantId(Integer.toString(tenantId));
+        tenantAwareLoggingEvent.setServiceName(serviceName);
+        return tenantAwareLoggingEvent;
     }
 
     public static org.apache.log4j.Level getLogLevel(java.util.logging.Level level) {
