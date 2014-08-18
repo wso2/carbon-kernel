cbf84e344230dbc60afc4071a89e98c439b6a632
shameera <shameerainfo@gmail.com>
2014-08-18 16:29:45 +0530

    Optimized cacheImpl imports

--- core/javax.cache/src/main/java/org/wso2/carbon/caching/impl/CacheImpl.java
+++ core/javax.cache/src/main/java/org/wso2/carbon/caching/impl/CacheImpl.java
@@ -55,7 +55,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 /**
  * TODO: class description
