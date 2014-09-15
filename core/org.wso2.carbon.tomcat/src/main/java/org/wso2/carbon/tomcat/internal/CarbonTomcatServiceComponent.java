b4ec2b959505b876896e09b65d705f86034c4a39
denuwanthi <denuwanthi.hasanthika@gmail.com>
2014-09-15 10:08:21 +0530

    add a service component, dataholder to get sci info & class which extends COntextCOnfig

---
+++ core/org.wso2.carbon.tomcat/src/main/java/org/wso2/carbon/tomcat/internal/CarbonTomcatServiceComponent.java
@@ -0,0 +1,36 @@
+package org.wso2.carbon.tomcat.internal;/*
+*Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+
+import javax.servlet.ServletContainerInitializer;
+
+/**
+ * @scr.component name="org.wso2.carbon.tomcat.internal.CarbonTomcatServiceComponent" immediate="true"
+ *
+ * @scr.reference name="sci" interface="javax.servlet.ServletContainerInitializer"
+ * cardinality="0..n" policy="dynamic" bind="setServletContainerInitializer" unbind="unsetServletContainerInitializer"
+ */
+public class CarbonTomcatServiceComponent {
+
+    public void setServletContainerInitializer(ServletContainerInitializer sci){
+        DataHolder.getInstance().addServletContainerInitializer(sci);
+    }
+
+    public void unsetServletContainerInitializer(ServletContainerInitializer sci){
+        DataHolder.getInstance().removeServletContainerInitializer(sci);
+    }
+}
