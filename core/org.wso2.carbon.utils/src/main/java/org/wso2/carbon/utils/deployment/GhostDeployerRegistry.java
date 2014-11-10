6a8b5238aaaafd270935701c04a09728a8ab5940
nipuni <nipuni880917@gmail.com>
2014-11-10 16:27:09 +0530

    updating fix for WSAS-1727

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/deployment/GhostDeployerRegistry.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/deployment/GhostDeployerRegistry.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2005-2011 WSO2, Inc. (http://wso2.com)
+ * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.utils.deployment;
@@ -75,8 +77,15 @@ public class GhostDeployerRegistry implements BundleListener {
             register(bundle);
         }
         for (DeployerConfig deployerConfig : deployerConfigs) {
-            Deployer deployer = getDeployer(deployerConfig.getClassStr());
-            addDeployer(deployerConfig, deployer);
+            try {
+                Deployer deployer = CarbonUtils.getDeployer(deployerConfig.getClassStr());
+                addDeployer(deployerConfig, deployer);
+            } catch (Exception e) {
+                //logging error and continue
+                //Exceptions in here, are due to issues with reading deployers
+                //they will handled by CarbonUtils hence continue.
+                log.error("Error reading deployer from deployer condfigs", e);
+            }
         }
         /**
          * Axis2 DeploymentEngine has made the AAR deployer a special case by hardcoding it.
@@ -149,21 +158,6 @@ public class GhostDeployerRegistry implements BundleListener {
 
     }
 
-    private Deployer getDeployer(String className) {
-        Deployer deployer = null;
-        try {
-            Class deployerClass = Class.forName(className);
-            deployer = (Deployer) deployerClass.newInstance();
-
-        } catch (ClassNotFoundException e) {
-            log.error("Deployer class not found ", e);
-        } catch (InstantiationException e) {
-            log.error("Cannot create new deployer instance", e);
-        } catch (IllegalAccessException e) {
-            log.error("Error creating deployer", e);
-        }
-        return deployer;
-    }
 
     /**
      * Unregister the deployer which has been registered by the given bundle
