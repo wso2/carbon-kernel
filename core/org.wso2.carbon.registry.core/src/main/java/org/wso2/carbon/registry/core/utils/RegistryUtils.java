e83816c9e5280ec161bd9c608914cdbf1c98fd0f
prasa7 <prasannadangalla@gmail.com>
2015-03-04 00:00:49 +0530

    Fixed incorrect exception handling.

--- core/org.wso2.carbon.registry.core/src/main/java/org/wso2/carbon/registry/core/utils/RegistryUtils.java
+++ core/org.wso2.carbon.registry.core/src/main/java/org/wso2/carbon/registry/core/utils/RegistryUtils.java
@@ -193,8 +193,8 @@ public final class RegistryUtils {
                 return (connectionMetaData.getUserName() != null ? connectionMetaData.getUserName().split("@")[0] :
                         connectionMetaData.getUserName()) + "@" + connectionMetaData.getURL();
             }
-        } catch (SQLException ignore) {
-            log.debug("Failed to construct the connectionId ." + ignore.getMessage());
+        } catch (SQLException e) {
+            log.error("Failed to construct the connectionId.", e);
         }
         return null;
     }
