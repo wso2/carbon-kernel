ac2ab2c247be077098cb23143dbeb6220884c3d3
Niranjan-K <niranjan.karu@gmail.com>
2015-05-08 11:32:00 +0530

    Fixed alignment

--- distribution/integration/tests-integration/src/test/java/org/wso2/carbon/integration/jira/issues/test/CARBON15203GSONBuilderOrderTestCase.java
+++ distribution/integration/tests-integration/src/test/java/org/wso2/carbon/integration/jira/issues/test/CARBON15203GSONBuilderOrderTestCase.java
@@ -22,7 +22,6 @@ import org.apache.commons.logging.LogFactory;
 import org.testng.annotations.*;
 import org.wso2.carbon.automation.engine.FrameworkConstants;
 import org.wso2.carbon.automation.extensions.servers.utils.ClientConnectionUtil;
-import org.wso2.carbon.automation.test.utils.common.TestConfigurationProvider;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 import org.wso2.carbon.integration.common.utils.CarbonIntegrationBaseTest;
 import org.wso2.carbon.automation.engine.context.AutomationContext;
@@ -39,137 +38,137 @@ import static org.testng.Assert.assertTrue;
 
 public class CARBON15203GSONBuilderOrderTestCase extends CarbonIntegrationBaseTest {
 
-	private static final Log log = LogFactory.getLog(CARBON15203GSONBuilderOrderTestCase.class);
-	private static int portOffset = 28;
-	private CarbonTestServerManager serverManager;
-	private AutomationContext context;
-	private String carbonHome;
+    private static final Log log = LogFactory.getLog(CARBON15203GSONBuilderOrderTestCase.class);
+    private static int portOffset = 28;
+    private CarbonTestServerManager serverManager;
+    private AutomationContext context;
+    private String carbonHome;
 
-	@BeforeClass(groups = { "carbon.integration.jira" })
-	public void initialize() throws Exception {
-		HashMap<String, String> startUpParameterMap = new HashMap<String, String>();
-		startUpParameterMap.put("-DportOffset", String.valueOf(portOffset));
-		context = new AutomationContext();
-		serverManager = new CarbonTestServerManager(context, System.getProperty("carbon.zip"), startUpParameterMap);
-		serverManager.startServer();
-		carbonHome = serverManager.getCarbonHome();
-		changeConfigFiles();
-		restartServer();
-	}
+    @BeforeClass(groups = { "carbon.integration.jira" })
+    public void initialize() throws Exception {
+        HashMap<String, String> startUpParameterMap = new HashMap<String, String>();
+        startUpParameterMap.put("-DportOffset", String.valueOf(portOffset));
+        context = new AutomationContext();
+        serverManager = new CarbonTestServerManager(context, System.getProperty("carbon.zip"), startUpParameterMap);
+        serverManager.startServer();
+        carbonHome = serverManager.getCarbonHome();
+        changeConfigFiles();
+        restartServer();
+    }
 
-	@AfterClass(groups = { "carbon.integration.jira" })
-	public void destroy() throws Exception {
-		if (serverManager != null) {
-			serverManager.stopServer();
-		}
-	}
+    @AfterClass(groups = { "carbon.integration.jira" })
+    public void destroy() throws Exception {
+        if (serverManager != null) {
+            serverManager.stopServer();
+        }
+    }
 
-	@Test(description = "Verify GSON Builder for DSS JSON")
-	public void dssGSONBuilderTest() throws Exception {
-		uploadApp();
-		String serviceEndpoint = "http://" + context.getInstance().getHosts().get("default") + ":" +
-		                         (Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTP_PORT) + portOffset) +
-		                         "/services/DssVerifierService/";
-		String endpoint = "verifyJsonRequest";
-		String contentType = "application/json";
-		String jsonRequest1 = "{\"" + endpoint + "\":{\"id\":291, \"name\":\"La Rochelle Gifts\", " +
-		                      "\"lastName\":\"Labrune\", \"firstName\":\"Janine\", \"age\":28, " +
-		                      "\"country\":\"France\"}}";
-		String jsonResponse1 = "{\"verifyJsonRequestResponse\":{\"return\":\"Success\"}}";
-		HttpResponse response1 = this.getHttpResponse(serviceEndpoint + endpoint, contentType, jsonRequest1);
-		assertTrue(response1.getData().equalsIgnoreCase(jsonResponse1));
+    @Test(description = "Verify GSON Builder for DSS JSON")
+    public void dssGSONBuilderTest() throws Exception {
+        uploadApp();
+        String serviceEndpoint = "http://" + context.getInstance().getHosts().get("default") + ":" +
+                                 (Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTP_PORT) + portOffset) +
+                                 "/services/DssVerifierService/";
+        String endpoint = "verifyJsonRequest";
+        String contentType = "application/json";
+        String jsonRequest1 = "{\"" + endpoint + "\":{\"id\":291, \"name\":\"La Rochelle Gifts\", " +
+                              "\"lastName\":\"Labrune\", \"firstName\":\"Janine\", \"age\":28, " +
+                              "\"country\":\"France\"}}";
+        String jsonResponse1 = "{\"verifyJsonRequestResponse\":{\"return\":\"Success\"}}";
+        HttpResponse response1 = this.getHttpResponse(serviceEndpoint + endpoint, contentType, jsonRequest1);
+        assertTrue(response1.getData().equalsIgnoreCase(jsonResponse1));
 
-		String jsonRequest2 = "{\"" + endpoint + "\":{\"age\":28, \"name\":\"La Rochelle Gifts\", " +
-		                      "\"lastName\":\"Labrune\", \"firstName\":\"Janine\", \"id\":291, " +
-		                      "\"country\":\"France\"}}";
-		String jsonResponse2 = "{\"Fault\":{\"faultcode\":\"soapenv:Server\",\"faultstring\":\"6\",\"detail\":\"\"}}";
-		HttpResponse response2 = this.getHttpResponse(serviceEndpoint + endpoint, contentType, jsonRequest2);
-		assertTrue(response2.getData().equalsIgnoreCase(jsonResponse2));
-	}
+        String jsonRequest2 = "{\"" + endpoint + "\":{\"age\":28, \"name\":\"La Rochelle Gifts\", " +
+                              "\"lastName\":\"Labrune\", \"firstName\":\"Janine\", \"id\":291, " +
+                              "\"country\":\"France\"}}";
+        String jsonResponse2 = "{\"Fault\":{\"faultcode\":\"soapenv:Server\",\"faultstring\":\"6\",\"detail\":\"\"}}";
+        HttpResponse response2 = this.getHttpResponse(serviceEndpoint + endpoint, contentType, jsonRequest2);
+        assertTrue(response2.getData().equalsIgnoreCase(jsonResponse2));
+    }
 
-	private void changeConfigFiles() throws Exception {
-		String axis2Artifact =
-				System.getProperty(FrameworkConstants.SYSTEM_ARTIFACT_RESOURCE_LOCATION) + File.separator +
-				"axis2config" + File.separator + "CARBON15203" + File.separator + "axis2.xml";
-		File axis2SrcFile = new File(axis2Artifact);
-		String axis2ConfigPath =
-				carbonHome + File.separator + "repository" + File.separator + "conf" + File.separator + "axis2";
-		File axis2File = new File(axis2ConfigPath + File.separator + "axis2.xml");
-		FileManipulator.copyFile(axis2SrcFile, axis2File);
-	}
+    private void changeConfigFiles() throws Exception {
+        String axis2Artifact =
+                System.getProperty(FrameworkConstants.SYSTEM_ARTIFACT_RESOURCE_LOCATION) + File.separator +
+                "axis2config" + File.separator + "CARBON15203" + File.separator + "axis2.xml";
+        File axis2SrcFile = new File(axis2Artifact);
+        String axis2ConfigPath =
+                carbonHome + File.separator + "repository" + File.separator + "conf" + File.separator + "axis2";
+        File axis2File = new File(axis2ConfigPath + File.separator + "axis2.xml");
+        FileManipulator.copyFile(axis2SrcFile, axis2File);
+    }
 
-	private void restartServer() throws Exception {
-		ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTPS_PORT) + portOffset,
-		                                 context.getInstance().getHosts().get("default"));
-		ServerAdminClient serverAdminClient = new ServerAdminClient(
-				"https://" + context.getInstance().getHosts().get("default") + ":" +
-				(Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTPS_PORT) + portOffset) +
-				"/services/ServerAdmin/", context.getSuperTenant().getTenantAdmin().getUserName(),
-				context.getSuperTenant().getTenantAdmin().getPassword());
-		serverAdminClient.restart();
-	}
+    private void restartServer() throws Exception {
+        ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTPS_PORT) + portOffset,
+                                         context.getInstance().getHosts().get("default"));
+        ServerAdminClient serverAdminClient = new ServerAdminClient(
+                "https://" + context.getInstance().getHosts().get("default") + ":" +
+                (Integer.parseInt(FrameworkConstants.SERVER_DEFAULT_HTTPS_PORT) + portOffset) +
+                "/services/ServerAdmin/", context.getSuperTenant().getTenantAdmin().getUserName(),
+                context.getSuperTenant().getTenantAdmin().getPassword());
+        serverAdminClient.restart();
+    }
 
-	private void uploadApp() throws Exception {
-		String aarServiceFile = "Axis2SampleService.aar";
-		String axis2SampleServiceDir = System.getProperty("axis2.sample.service.dir");
-		if (axis2SampleServiceDir == null || !(new File(axis2SampleServiceDir)).exists()) {
-			log.warn("DSS JSON verification test not enabled");
-			return;
-		}
-		assert carbonHome != null : "carbonHome cannot be null";
-		File srcFile = new File(axis2SampleServiceDir + aarServiceFile);
-		assert srcFile.exists() : srcFile.getAbsolutePath() + " does not exist";
-		String deploymentPath =
-				carbonHome + File.separator + "repository" + File.separator + "deployment" + File.separator + "server" +
-				File.separator + "axis2services";
-		File depFile = new File(deploymentPath);
-		if (!depFile.exists() && !depFile.mkdir()) {
-			throw new IOException("Error while creating the deployment folder : " + deploymentPath);
-		}
-		File dstFile = new File(depFile.getAbsoluteFile() + File.separator + aarServiceFile);
-		log.info("Copying " + srcFile.getAbsolutePath() + " => " + dstFile.getAbsolutePath());
-		FileManipulator.copyFile(srcFile, dstFile);
-		Thread.sleep(20000);
-	}
+    private void uploadApp() throws Exception {
+        String aarServiceFile = "Axis2SampleService.aar";
+        String axis2SampleServiceDir = System.getProperty("axis2.sample.service.dir");
+        if (axis2SampleServiceDir == null || !(new File(axis2SampleServiceDir)).exists()) {
+            log.warn("DSS JSON verification test not enabled");
+            return;
+        }
+        assert carbonHome != null : "carbonHome cannot be null";
+        File srcFile = new File(axis2SampleServiceDir + aarServiceFile);
+        assert srcFile.exists() : srcFile.getAbsolutePath() + " does not exist";
+        String deploymentPath =
+                carbonHome + File.separator + "repository" + File.separator + "deployment" + File.separator + "server" +
+                File.separator + "axis2services";
+        File depFile = new File(deploymentPath);
+        if (!depFile.exists() && !depFile.mkdir()) {
+            throw new IOException("Error while creating the deployment folder : " + deploymentPath);
+        }
+        File dstFile = new File(depFile.getAbsoluteFile() + File.separator + aarServiceFile);
+        log.info("Copying " + srcFile.getAbsolutePath() + " => " + dstFile.getAbsolutePath());
+        FileManipulator.copyFile(srcFile, dstFile);
+        Thread.sleep(20000);
+    }
 
-	HttpResponse getHttpResponse(String endpoint, String contentType, String data) throws Exception {
-		if (endpoint.startsWith("http://")) {
-			URL url = new URL(endpoint);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Content-Type", contentType);
-			conn.setRequestProperty("Accept", contentType);
-			conn.setRequestProperty("charset", "UTF-8");
-			conn.setReadTimeout(10000);
-			conn.setRequestProperty("Content-Length", String.valueOf(data.length()));
-			OutputStream os = conn.getOutputStream();
-			os.write(data.getBytes());
-			os.close();
-			conn.connect();
-			// Get the response
-			StringBuilder sb = new StringBuilder();
-			BufferedReader rd = null;
-			try {
-				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-				String line;
-				while ((line = rd.readLine()) != null) {
-					sb.append(line);
-				}
-			} catch (IOException ignored) {
-				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-				String line;
-				while ((line = rd.readLine()) != null) {
-					sb.append(line);
-				}
-			} finally {
-				if (rd != null) {
-					rd.close();
-				}
-			}
-			return new HttpResponse(sb.toString(), conn.getResponseCode());
-		}
-		return null;
-	}
+    HttpResponse getHttpResponse(String endpoint, String contentType, String data) throws Exception {
+        if (endpoint.startsWith("http://")) {
+            URL url = new URL(endpoint);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", contentType);
+            conn.setRequestProperty("Accept", contentType);
+            conn.setRequestProperty("charset", "UTF-8");
+            conn.setReadTimeout(10000);
+            conn.setRequestProperty("Content-Length", String.valueOf(data.length()));
+            OutputStream os = conn.getOutputStream();
+            os.write(data.getBytes());
+            os.close();
+            conn.connect();
+            // Get the response
+            StringBuilder sb = new StringBuilder();
+            BufferedReader rd = null;
+            try {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException ignored) {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+            } finally {
+                if (rd != null) {
+                    rd.close();
+                }
+            }
+            return new HttpResponse(sb.toString(), conn.getResponseCode());
+        }
+        return null;
+    }
 
 }
