c79ebaea85807c48175230a7b27c68b5487d94f2
mirhamiqbal <irham0019@gmail.com>
2015-06-30 15:37:02 +0530

    code formatting and throw exceptions without catching in the tests and admin clients

--- distribution/integration/test-common/admin-clients/src/main/java/org/wso2/carbon/integration/common/clients/FeatureAdminClient.java
+++ distribution/integration/test-common/admin-clients/src/main/java/org/wso2/carbon/integration/common/clients/FeatureAdminClient.java
@@ -18,23 +18,23 @@
 package org.wso2.carbon.integration.common.clients;
 
 import org.apache.axis2.AxisFault;
-import org.apache.axis2.Constants;
-import org.apache.axis2.client.Options;
-import org.apache.axis2.client.ServiceClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.automation.engine.context.AutomationContext;
 import org.wso2.carbon.feature.mgt.core.operations.OperationFactory;
-import org.wso2.carbon.feature.mgt.stub.ProvisioningAdminServiceCallbackHandler;
 import org.wso2.carbon.feature.mgt.stub.ProvisioningAdminServiceStub;
-import org.wso2.carbon.feature.mgt.stub.prov.data.*;
+import org.wso2.carbon.feature.mgt.stub.prov.data.Feature;
+import org.wso2.carbon.feature.mgt.stub.prov.data.FeatureInfo;
+import org.wso2.carbon.feature.mgt.stub.prov.data.LicenseInfo;
+import org.wso2.carbon.feature.mgt.stub.prov.data.ProfileHistory;
+import org.wso2.carbon.feature.mgt.stub.prov.data.ProvisioningActionInfo;
+import org.wso2.carbon.feature.mgt.stub.prov.data.ProvisioningActionResultInfo;
 import org.wso2.carbon.feature.mgt.ui.FeatureWrapper;
 import org.wso2.carbon.feature.mgt.ui.util.Utils;
 import org.wso2.carbon.integration.framework.utils.AuthenticateStubUtil;
 import org.wso2.carbon.utils.CarbonUtils;
 
 import javax.xml.xpath.XPathExpressionException;
-import java.util.Arrays;
+import java.rmi.RemoteException;
 
 public class FeatureAdminClient {
 
@@ -49,131 +49,74 @@ public class FeatureAdminClient {
 
     private ProfileHistory[] profileHistories = new ProfileHistory[]{};
 
-    public FeatureAdminClient(String backendURL, AutomationContext automationContext, String sessionCookie) throws AxisFault,
-            XPathExpressionException {
+    public FeatureAdminClient(String backendURL, String sessionCookie) throws AxisFault,
+                                                                              XPathExpressionException {
         String endPoint = backendURL + serviceName;
         provAdminStub = new ProvisioningAdminServiceStub(endPoint);
         AuthenticateStubUtil.authenticateStub(sessionCookie, provAdminStub);
     }
 
-    public ProvisioningActionResultInfo reviewInstallFeaturesAction(FeatureInfo[] features) throws Exception {
+    public ProvisioningActionResultInfo reviewInstallFeaturesAction(FeatureInfo[] features)
+            throws RemoteException {
         ProvisioningActionResultInfo provisioningActionResultInfo = null;
-        try {
-            ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
-            provisioningActionInfo.setFeaturesToInstall(features);
-            provisioningActionInfo.setActionType(OperationFactory.INSTALL_ACTION);
-            provisioningActionResultInfo = provAdminStub.reviewProvisioningAction(provisioningActionInfo);
-        } catch (AxisFault e) {
-            handleException("failed.review.prov.action", e);
-        }
+        ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
+        provisioningActionInfo.setFeaturesToInstall(features);
+        provisioningActionInfo.setActionType(OperationFactory.INSTALL_ACTION);
+        provisioningActionResultInfo = provAdminStub.reviewProvisioningAction(provisioningActionInfo);
         return provisioningActionResultInfo;
     }
 
-    public FeatureInfo getInstalledFeatureDetails(String featureID, String featureVersion) throws Exception {
-        FeatureInfo featureInfo = null;
-        try {
-            featureInfo = provAdminStub.getInstalledFeatureInfo(featureID, featureVersion);
-        } catch (AxisFault e) {
-            handleException("failed.get.feature.information", e);
-        }
-        return featureInfo;
+    public FeatureInfo getInstalledFeatureDetails(String featureID, String featureVersion)
+            throws RemoteException {
+        return provAdminStub.getInstalledFeatureInfo(featureID, featureVersion);
     }
 
     public LicenseInfo[] getLicensingInformation() throws Exception {
-        LicenseInfo[] licenseInfo = null;
-        try {
-            licenseInfo = provAdminStub.getLicensingInformation();
-        } catch (AxisFault e) {
-            handleException("failed.get.license.info", e);
-        }
-        return licenseInfo;
+        return provAdminStub.getLicensingInformation();
     }
 
     public void performInstallation(String actionType) throws Exception {
-        try {
             provAdminStub.performProvisioningAction(actionType);
-
-        } catch (AxisFault e) {
-            handleException("failed.perform.prov.action", e);
-        }
     }
 
     public FeatureWrapper[] getInstalledFeatures() throws Exception {
-        FeatureWrapper[] featureWrappers = null;
-        try {
             Feature[] features = provAdminStub.getAllInstalledFeatures();
-            featureWrappers = Utils.processFeatureTree(features, false, 0);
-        } catch (AxisFault e) {
-            handleException("failed.get.installed.features", e);
-        }
-        return featureWrappers;
+        return Utils.processFeatureTree(features, false, 0);
     }
 
-    public FeatureInfo[] getInstalledFeaturesWithProperty(String key, String value) throws Exception {
-        FeatureInfo[] featureInfos = null;
-        try {
-            featureInfos = provAdminStub.getInstalledFeaturesWithProperty(key, value);
-        } catch (AxisFault e) {
-            handleException("failed.get.installed.feature.with.prop", e);
-        }
-        return featureInfos;
+    public FeatureInfo[] getInstalledFeaturesWithProperty(String key, String value)
+            throws RemoteException {
+        return provAdminStub.getInstalledFeaturesWithProperty(key, value);
     }
 
-    public ProvisioningActionResultInfo reviewUninstallFeaturesAction(FeatureInfo[] features) throws Exception {
-        ProvisioningActionResultInfo provisioningActionResultInfo = null;
-        try {
-            ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
-            provisioningActionInfo.setFeaturesToUninstall(features);
-            provisioningActionInfo.setActionType(OperationFactory.UNINSTALL_ACTION);
-            return provAdminStub.reviewProvisioningAction(provisioningActionInfo);
-        } catch (AxisFault e) {
-            handleException("failed.review.prov.action", e);
-        }
-        return provisioningActionResultInfo;
+    public ProvisioningActionResultInfo reviewUninstallFeaturesAction(FeatureInfo[] features)
+            throws RemoteException {
+        ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
+        provisioningActionInfo.setFeaturesToUninstall(features);
+        provisioningActionInfo.setActionType(OperationFactory.UNINSTALL_ACTION);
+        return provAdminStub.reviewProvisioningAction(provisioningActionInfo);
     }
 
     public ProfileHistory[] getProfileHistory() throws Exception {
-        try {
             if (CarbonUtils.isRunningOnLocalTransportMode()) {
                 profileHistories = provAdminStub.getProfileHistory();
-            } else {
-                ServiceClient client = provAdminStub._getServiceClient();
-                client.engageModule("addressing"); // IMPORTANT
-                Options options = client.getOptions();
-                options.setUseSeparateListener(true);
-                options.setTransportInProtocol(Constants.TRANSPORT_HTTP);
-                provAdminStub.startgetProfileHistory(callback);
-                handleCallback();
             }
-        } catch (AxisFault e) {
-            handleException("failed.get.profile.history", e);
-        }
         return profileHistories;
     }
 
     public ProvisioningActionResultInfo reviewRevertPlan(String timestampString) throws Exception {
-        ProvisioningActionResultInfo provisioningActionResultInfo = null;
-        try {
-            ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
-            provisioningActionInfo.setTimestamp(Long.parseLong(timestampString));
-            provisioningActionInfo.setActionType(OperationFactory.REVERT_ACTION);
-            provisioningActionResultInfo = provAdminStub.reviewProvisioningAction(provisioningActionInfo);
-        } catch (AxisFault e) {
-            handleException("failed.review.prov.action", e);
-        }
-        return provisioningActionResultInfo;
+        ProvisioningActionInfo provisioningActionInfo = new ProvisioningActionInfo();
+        provisioningActionInfo.setTimestamp(Long.parseLong(timestampString));
+        provisioningActionInfo.setActionType(OperationFactory.REVERT_ACTION);
+        return provAdminStub.reviewProvisioningAction(provisioningActionInfo);
     }
 
     public void removeAllFeaturesWithProperty(String key, String value) throws Exception {
-        try {
             if ("org.wso2.carbon.p2.category.type".equals(key) && "server".equalsIgnoreCase(value)) {
                 provAdminStub.removeAllServerFeatures();
             } else if ("org.wso2.carbon.p2.category.type".equals(key) && "console".equalsIgnoreCase(value)) {
                 provAdminStub.removeAllConsoleFeatures();
             }
-        } catch (AxisFault e) {
-            handleException("failed.remove.server.console.features", e);
-        }
     }
 
     public String getInstallActionType() {
@@ -204,58 +147,6 @@ public class FeatureAdminClient {
         } else {
             isComplete = false;
         }
-    }
-
-    ProvisioningAdminServiceCallbackHandler callback = new ProvisioningAdminServiceCallbackHandler() {
-        @Override
-        public void receiveResultperformProvisioningAction(boolean result) {
-            isComplete = true;
-        }
-
-        @Override
-        public void receiveErrorperformProvisioningAction(Exception e) {
-            isError = true;
-            exception = e;
-        }
-
-        @Override
-        public void receiveResultremoveAllConsoleFeatures(boolean result) {
-            isComplete = true;
-        }
-
-        @Override
-        public void receiveErrorremoveAllConsoleFeatures(Exception e) {
-            isError = true;
-            exception = e;
-        }
-
-        @Override
-        public void receiveResultremoveAllServerFeatures(boolean result) {
-            isComplete = true;
-        }
-
-        @Override
-        public void receiveErrorremoveAllServerFeatures(Exception e) {
-            isError = true;
-            exception = e;
-        }
-
-        @Override
-        public void receiveResultgetProfileHistory(ProfileHistory[] result) {
-            profileHistories = Arrays.copyOf(result, result.length);
-            isComplete = true;
-        }
-
-        @Override
-        public void receiveErrorgetProfileHistory(Exception e) {
-            isError = true;
-            exception = e;
-        }
-    };
-
-    private void handleException(String msg, Exception e) throws Exception {
-        log.error(msg, e);
-        throw new Exception(msg, e);
     }
 
 }
