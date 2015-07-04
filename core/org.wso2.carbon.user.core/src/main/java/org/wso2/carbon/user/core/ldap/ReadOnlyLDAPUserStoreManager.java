84666d439900e1dc4809916b4fcc75a304a6cc9e
Damith Senanayake <dracusds123@gmail.com>
2015-07-04 16:35:41 +0530

    now escaping special characters in usernames

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/ReadOnlyLDAPUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/ReadOnlyLDAPUserStoreManager.java
@@ -1781,8 +1781,7 @@ public class ReadOnlyLDAPUserStoreManager extends AbstractUserStoreManager {
                         // create DN directly   but there is no way when multiple DNs are used. Need to improve letter
                         String userDNPattern = realmConfig.getUserStoreProperty(LDAPConstants.USER_DN_PATTERN);
                         if (userDNPattern != null & !"".equals(userDNPattern) && !userDNPattern.contains("#")) {
-                            searchBase = MessageFormat.format(userDNPattern, userName);
-
+                            searchBase = MessageFormat.format(userDNPattern, escapeSpecialCharactersForDN(userName));
                         }
                     }
 
@@ -1820,8 +1819,7 @@ public class ReadOnlyLDAPUserStoreManager extends AbstractUserStoreManager {
                 String userDNPattern = realmConfig.getUserStoreProperty(LDAPConstants.USER_DN_PATTERN);
                 String nameInSpace;
                 if (userDNPattern != null && !"".equals(userDNPattern) && !userDNPattern.contains("#")) {
-                    nameInSpace = MessageFormat.format(userDNPattern, userName);
-
+                    nameInSpace = MessageFormat.format(userDNPattern, escapeSpecialCharactersForDN(userName));
                 } else {
                     nameInSpace = this.getNameInSpaceForUserName(userName);
                 }
