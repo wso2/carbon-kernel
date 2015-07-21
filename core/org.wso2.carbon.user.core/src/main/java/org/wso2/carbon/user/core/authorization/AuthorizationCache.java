5dee34bcdbf376cdec3d3aba597af407381e6e3b
Thusitha Dayaratne <thusithad@wso2.com>
2015-07-21 12:16:19 +0530

    Resolve conflicts when reverting 0a1eb29f49c1b4b77d9fac13fd968d522f2ff093

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/authorization/AuthorizationCache.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/authorization/AuthorizationCache.java
@@ -17,11 +17,6 @@ package org.wso2.carbon.user.core.authorization;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.user.api.UserStoreException;
-import org.wso2.carbon.user.core.UserCoreConstants;
-import org.wso2.carbon.user.core.UserStoreManager;
-import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
-import org.wso2.carbon.user.core.internal.UserStoreMgtDSComponent;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
@@ -107,9 +102,6 @@ public class AuthorizationCache {
     public void addToCache(String serverId, int tenantId, String userName,
                            String resourceId, String action, boolean isAuthorized) {
 
-        if (!isUsernameCaseSensitive(userName, tenantId)){
-            userName = userName.toLowerCase();
-        }
         // Element already in the cache. Remove it first
         clearCacheEntry(serverId, tenantId, userName, resourceId, action);
 
@@ -146,9 +138,6 @@ public class AuthorizationCache {
         if (isCacheNull(cache)) {
             throw new AuthorizationCacheException(
                     "Authorization information not found in the cache.");
-        }
-        if (!isUsernameCaseSensitive(userName, tenantId)){
-            userName = userName.toLowerCase();
         }
 
         AuthorizationKey key = new AuthorizationKey(serverId, tenantId,
@@ -196,10 +185,6 @@ public class AuthorizationCache {
             return;
         }
 
-        if (!isUsernameCaseSensitive(userName, tenantId)){
-            userName = userName.toLowerCase();
-        }
-
         AuthorizationKey key = new AuthorizationKey(serverId, tenantId,
                 userName, resourceId, action);
         if (cache.containsKey(key)) {
@@ -218,9 +203,6 @@ public class AuthorizationCache {
         // check for null
         if (isCacheNull(cache)) {
             return;
-        }
-        if (!isUsernameCaseSensitive(userName, tenantId)){
-            userName = userName.toLowerCase();
         }
         for (Cache.Entry<AuthorizationKey, AuthorizeCacheEntry> entry : cache) {
             AuthorizationKey authorizationKey = entry.getKey();
@@ -322,37 +304,5 @@ public class AuthorizationCache {
      */
     public void disableCache() {
         isEnable = false;
-    }
-
-    private boolean isUsernameCaseSensitive(String username, int tenantId){
-        if (UserStoreMgtDSComponent.getRealmService()!= null) {
-            //this check is added to avoid NullPointerExceptions if the osgi is not started yet.
-            //as an example when running the unit tests.
-            try {
-                UserStoreManager userStoreManager = (UserStoreManager) UserStoreMgtDSComponent.getRealmService()
-                        .getTenantUserRealm(tenantId).getUserStoreManager();
-                UserStoreManager userAvailableUserStoreManager = userStoreManager.getSecondaryUserStoreManager
-                        (getDomainFromName(username));
-                if (userAvailableUserStoreManager instanceof AbstractUserStoreManager) {
-                    return ((AbstractUserStoreManager) userAvailableUserStoreManager).isCaseSensitiveUsername();
-                } else {
-                    return false;
-                }
-            } catch (UserStoreException e) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Error while reading user store property CaseSensitiveUsername. Considering as false.");
-                }
-            }
-        }
-        return false;
-    }
-
-    private String getDomainFromName(String name) {
-        int index;
-        if ((index = name.indexOf("/")) > 0) {
-            String domain = name.substring(0, index);
-            return domain;
-        }
-        return UserCoreConstants.PRIMARY_DEFAULT_DOMAIN_NAME;
     }
 }
