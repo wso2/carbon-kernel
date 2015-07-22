dca4416c4015e6c6f3724685c27ed90647c197b8
Thusitha Dayaratne <thusithad@wso2.com>
2015-07-22 18:34:41 +0530

    Fixing conflicts while reverting API changes

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/UserStoreConfigConstants.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/UserStoreConfigConstants.java
@@ -36,9 +36,7 @@ public class UserStoreConfigConstants {
     public static final String userRolesCacheEnabledDescription = "This is to indicate whether to cache the role list of a user";
     public static final String SCIMEnabled = "SCIMEnabled";
     public static final String SCIMEnabledDescription = "Whether SCIM is enabled for the user store";
-    public static final String CASE_INSENSITIVE_USERNAME = "CaseInsensitiveUsername";
-    public static final String CASE_INSENSITIVE_USERNAME_DESCRIPTION = "Whether the username is case insensitive or not";
-
+    
     // JDBC User Store Specific Properties
     public static final String CASE_INSENSITIVE_USERNAME = "CaseInsensitiveUsername";
     public static final String CASE_INSENSITIVE_USERNAME_DESCRIPTION = "Whether the username is case insensitive or not";
