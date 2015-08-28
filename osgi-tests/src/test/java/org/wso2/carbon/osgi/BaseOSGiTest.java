b9d8f0b5b1ff7251e8a81a6faae6603814a72875
arunasujith <sujithucsc@gmail.com>
2015-08-28 11:40:21 +0530

    fixing checkstyle issues

--- osgi-tests/src/test/java/org/wso2/carbon/osgi/BaseOSGiTest.java
+++ osgi-tests/src/test/java/org/wso2/carbon/osgi/BaseOSGiTest.java
@@ -30,7 +30,9 @@ import org.wso2.carbon.osgi.util.Utils;
 
 import javax.inject.Inject;
 
-import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.repositories;
 
 
 @Listeners(PaxExam.class)
@@ -55,34 +57,55 @@ public class BaseOSGiTest {
                 mavenBundle().artifactId("testng").groupId("org.testng").versionAsInProject(),
                 mavenBundle().artifactId("org.wso2.carbon.core").groupId("org.wso2.carbon").versionAsInProject(),
                 mavenBundle().artifactId("hazelcast").groupId("org.wso2.orbit.com.hazelcast").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator").groupId("org.eclipse.equinox").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
                 mavenBundle().artifactId("javax.servlet-api").groupId("javax.servlet").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.command").groupId("org.apache.felix").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.runtime").groupId("org.apache.felix").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.expressions").groupId("org.eclipse.core").versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.gogo.command").groupId("org.apache.felix").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.gogo.runtime").groupId("org.apache.felix").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.core.expressions").groupId("org.eclipse.core").
+                        versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.core.jobs").groupId("org.eclipse.core").versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.core.runtime").groupId("org.eclipse.core").versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.ecf").groupId("org.eclipse.ecf").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf.filetransfer").groupId("org.eclipse.ecf").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.ecf.filetransfer").groupId("org.eclipse.ecf").
+                        versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.ecf.identity").groupId("org.eclipse.ecf").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf.provider.filetransfer").groupId("org.eclipse.ecf").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.ecf.provider.filetransfer").groupId("org.eclipse.ecf").
+                        versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.equinox.app").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.common").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.concurrent").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.console").groupId("org.eclipse.equinox").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.common").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.concurrent").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.console").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.equinox.ds").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin.equinox").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.http.servlet").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.launcher").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.preferences").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.registry").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator.manipulator").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.util").groupId("org.eclipse.equinox").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin.equinox").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.http.servlet").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.launcher").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.preferences").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.registry").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator.manipulator").
+                        groupId("org.eclipse.equinox").versionAsInProject(),
+                mavenBundle().artifactId("org.eclipse.equinox.util").groupId("org.eclipse.equinox").
+                        versionAsInProject(),
                 mavenBundle().artifactId("org.eclipse.osgi.services").groupId("org.eclipse.osgi").versionAsInProject()
         );
     }
@@ -96,11 +119,9 @@ public class BaseOSGiTest {
     public void testCarbonCoreBundleStatus() {
 
         Bundle coreBundle = null;
-
-        Bundle bundles[] = bundleContext.getBundles();
-        for (int i = 0; i < bundles.length; i++) {
-            if (bundles[i].getSymbolicName().equals("org.wso2.carbon.core")) {
-                coreBundle = bundles[i];
+        for (Bundle bundle : bundleContext.getBundles()) {
+            if (bundle.getSymbolicName().equals("org.wso2.carbon.core")) {
+                coreBundle = bundle;
                 break;
             }
         }
