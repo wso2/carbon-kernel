c9a79760d9134fb13c40c16f1fc35ebac4d15a40
arunasujith <sujithucsc@gmail.com>
2015-09-01 16:47:08 +0530

    moving configuration to a single class

--- osgi-tests/src/test/java/org/wso2/carbon/osgi/BaseOSGiTest.java
+++ osgi-tests/src/test/java/org/wso2/carbon/osgi/BaseOSGiTest.java
@@ -16,8 +16,6 @@
 
 package org.wso2.carbon.osgi;
 
-import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.ops4j.pax.exam.testng.listener.PaxExam;
@@ -26,13 +24,8 @@ import org.osgi.framework.BundleContext;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import org.wso2.carbon.osgi.util.Utils;
 
 import javax.inject.Inject;
-
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.repositories;
 
 
 @Listeners(PaxExam.class)
@@ -41,74 +34,6 @@ public class BaseOSGiTest {
 
     @Inject
     private BundleContext bundleContext;
-
-    //setting up the environment
-    private void setup() {
-        Utils.setCarbonHome();
-    }
-
-    @Configuration
-    public Option[] config() {
-        setup();
-
-        return options(
-                repositories("http://maven.wso2.org/nexus/content/groups/wso2-public"),
-                //must install the testng bundle
-                mavenBundle().artifactId("testng").groupId("org.testng").versionAsInProject(),
-                mavenBundle().artifactId("org.wso2.carbon.core").groupId("org.wso2.carbon").versionAsInProject(),
-                mavenBundle().artifactId("hazelcast").groupId("org.wso2.orbit.com.hazelcast").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("javax.servlet-api").groupId("javax.servlet").versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.command").groupId("org.apache.felix").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.runtime").groupId("org.apache.felix").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.apache.felix.gogo.shell").groupId("org.apache.felix").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.contenttype").groupId("org.eclipse.core").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.expressions").groupId("org.eclipse.core").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.jobs").groupId("org.eclipse.core").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.core.runtime").groupId("org.eclipse.core").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf").groupId("org.eclipse.ecf").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf.filetransfer").groupId("org.eclipse.ecf").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf.identity").groupId("org.eclipse.ecf").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.ecf.provider.filetransfer").groupId("org.eclipse.ecf").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.app").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.common").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.concurrent").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.console").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.ds").groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.frameworkadmin.equinox").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.http.servlet").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.launcher").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.preferences").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.registry").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.simpleconfigurator.manipulator").
-                        groupId("org.eclipse.equinox").versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.equinox.util").groupId("org.eclipse.equinox").
-                        versionAsInProject(),
-                mavenBundle().artifactId("org.eclipse.osgi.services").groupId("org.eclipse.osgi").versionAsInProject()
-        );
-    }
 
     @Test
     public void testBundleContextStatus() {
