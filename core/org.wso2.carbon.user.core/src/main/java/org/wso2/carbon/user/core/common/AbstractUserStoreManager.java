1c775a4d15e0eb2c5d41cb7bf226e5198169151a
IndunilR <indunil@wso2.com>
2015-10-15 12:47:33 +0530

    Resolved IDENTITY-3837

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
@@ -2685,7 +2685,7 @@ public abstract class AbstractUserStoreManager implements UserStoreManager {
                         log.error(e);
                     }
                 } else {
-                    storeManager.getRoleNames();
+                    roleList = UserCoreUtil.combineArrays(roleList, storeManager.getRoleNames());
                 }
             }
         }
