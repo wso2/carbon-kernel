7a7f7e70372827684c2aa59c390b3df1d7d44d17
dinushab <dinushaboteju@gmail.com>
2015-10-21 23:26:47 +0530

    CARBON-15553: Used Paths API instead if File.

--- core/src/test/java/org/wso2/carbon/utils/FileUtilsTest.java
+++ core/src/test/java/org/wso2/carbon/utils/FileUtilsTest.java
@@ -21,14 +21,13 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class FileUtilsTest {
 
-    private static File testDir = new File("target" + File.separator + "FileUtilTest");
-    private static File testSampleDirStructure = new File("target" + File.separator + "FileUtilTest" + File.separator +
-            "testSampleDirStructure");
-    private static File sampleTextFile = new File("src" + File.separator + "test" + File.separator + "resources" +
-        File.separator + "sample.txt");
+    private static File testDir = Paths.get("target", "FileUtilTest").toFile();
+    private static File testSampleDirStructure = Paths.get("target", "FileUtilTest", "testSampleDirStructure").toFile();
+    private static File sampleTextFile = Paths.get("src", "test", "resources", "sample.txt").toFile();
 
     @BeforeTest
     public void setup() {
@@ -46,8 +45,7 @@ public class FileUtilsTest {
 
     @Test
     public void testCopyFile() {
-        File destination = new File("target" + File.separator + "FileUtilTest" + File.separator +
-                "sample.txt");
+        File destination = Paths.get("target", "FileUtilTest", "sample.txt").toFile();
         try {
             FileUtils.copyFile(sampleTextFile, destination);
             Assert.assertTrue(destination.exists());
@@ -58,8 +56,7 @@ public class FileUtilsTest {
 
     @Test
     public void testCopyFileToDir() {
-        File destination = new File("target" + File.separator + "FileUtilTest" + File.separator +
-                "sample.txt");
+        File destination = Paths.get("target", "FileUtilTest", "sample.txt").toFile();
         try {
             FileUtils.copyFileToDir(sampleTextFile, testSampleDirStructure);
             Assert.assertTrue(destination.exists());
@@ -70,10 +67,8 @@ public class FileUtilsTest {
 
     @Test
     public void testArchiveDir() {
-        File destination = new File("target" + File.separator + "FileUtilTest" + File.separator +
-                "sample.txt");
         try {
-            File zipFile = new File(testDir.toString() + File.separator + "archive.zip");
+            File zipFile = Paths.get(testDir.toString(), "archive.zip").toFile();
             FileUtils.archiveDir(zipFile.getAbsolutePath(), testSampleDirStructure.getAbsolutePath());
             Assert.assertTrue(zipFile.exists());
         } catch (IOException e) {
