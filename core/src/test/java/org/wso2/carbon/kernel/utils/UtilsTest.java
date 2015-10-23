3101fa7060a50c80076a67ff0e56f1c656f4359a
Manuri <amaya.uom@gmail.com>
2015-10-23 13:56:32 +0530

    CARBON-15564: removing unused method

--- core/src/test/java/org/wso2/carbon/kernel/utils/UtilsTest.java
+++ core/src/test/java/org/wso2/carbon/kernel/utils/UtilsTest.java
@@ -12,20 +12,6 @@ import java.util.Map;
 
 public class UtilsTest {
 
-    /* private static void set(Map<String, String> newenv) throws Exception {
-         Class[] classes = Collections.class.getDeclaredClasses();
-         Map<String, String> env = System.getenv();
-         for (Class cl : classes) {
-             if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-                 Field field = cl.getDeclaredField("m");
-                 field.setAccessible(true);
-                 Object obj = field.get(env);
-                 Map<String, String> map = (Map<String, String>) obj;
-                 map.clear();
-                 map.putAll(newenv);
-             }
-         }
-     }*/
     protected static void set(Map<String, String> newenv) {
         try {
             Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
