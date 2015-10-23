5bd31df976af42b11a7a148b6da7bf2c53c90b6e
Manuri <amaya.uom@gmail.com>
2015-10-23 21:53:50 +0530

    CARBON-15572: Adding license and formatting

--- core/src/test/java/org/wso2/carbon/kernel/config/model/CarbonConfigurationTest.java
+++ core/src/test/java/org/wso2/carbon/kernel/config/model/CarbonConfigurationTest.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.wso2.carbon.kernel.config.model;
 
 import org.apache.commons.io.IOUtils;
@@ -37,32 +52,38 @@ public class CarbonConfigurationTest extends BaseTest {
         return (CarbonConfiguration) jaxbUnmarshaller.unmarshal(new StreamSource(new StringReader(xmlString)));
     }
 
-    @BeforeClass public void init() throws Exception {
+    @BeforeClass
+    public void init() throws Exception {
         try (InputStream inputStream = new FileInputStream(
                 new File(getTestResourceFile("xsd").getAbsolutePath() + File.separator + "carbon.xml"))) {
             carbonConfiguration = unmarshall(inputStream);
         }
     }
 
-    @Test public void testGetId() throws Exception {
+    @Test
+    public void testGetId() throws Exception {
         Assert.assertEquals(carbonConfiguration.getId(), "carbon-kernel");
     }
 
-    @Test public void testGetName() throws Exception {
+    @Test
+    public void testGetName() throws Exception {
         Assert.assertEquals(carbonConfiguration.getName(), "WSO2 Carbon Kernel");
 
     }
 
-    @Test public void testGetVersion() throws Exception {
+    @Test
+    public void testGetVersion() throws Exception {
         Assert.assertEquals(carbonConfiguration.getVersion(), "5.0.0-SNAPSHOT");
 
     }
 
-    @Test public void testGetPortsConfig() throws Exception {
+    @Test
+    public void testGetPortsConfig() throws Exception {
         Assert.assertEquals(carbonConfiguration.getPortsConfig().getOffset(), 0);
     }
 
-    @Test public void testGetDeploymentConfig() throws Exception {
+    @Test
+    public void testGetDeploymentConfig() throws Exception {
         DeploymentConfig deploymentConfig = carbonConfiguration.getDeploymentConfig();
         Assert.assertEquals(deploymentConfig.getMode(), DeploymentModeEnum.scheduled);
         Assert.assertEquals(deploymentConfig.getRepositoryLocation(), "repository-path");
