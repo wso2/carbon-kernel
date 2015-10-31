dcba2de60cf8b04da9650b074ca63f8e5190c430
dinushab <dinushaboteju@gmail.com>
2015-10-31 21:07:37 +0530

    Replaced possible for loops in CarbonServer.java with java 8 lambda expressions.

--- launcher/src/main/java/org/wso2/carbon/launcher/CarbonServer.java
+++ launcher/src/main/java/org/wso2/carbon/launcher/CarbonServer.java
@@ -163,7 +163,7 @@ public class CarbonServer {
     /**
      * Wait until this Framework has completely stopped.
      *
-     * @param framework osgi framework
+     * @param framework OSGi framework
      * @throws java.lang.Exception
      */
     private void waitForServerStop(Framework framework) throws Exception {
@@ -186,9 +186,9 @@ public class CarbonServer {
     }
 
     /**
-     * Create osgi framework class loader.
+     * Create OSGi framework class loader.
      *
-     * @return new osgi class loader
+     * @return new OSGi class loader
      */
     private ClassLoader createOSGiFwkClassLoader() {
         if (logger.isLoggable(Level.FINE)) {
@@ -201,7 +201,7 @@ public class CarbonServer {
 
     /**
      * Creates a new service loader for the given service type and class loader.
-     * Load osgi framework factory for the given class loader.
+     * Load OSGi framework factory for the given class loader.
      *
      * @param classLoader The class loader to be used to load provider-configurations
      * @return framework factory for creating framework instances
@@ -257,12 +257,12 @@ public class CarbonServer {
      */
     private void dispatchEvent(int event) {
         CarbonServerEvent carbonServerEvent = new CarbonServerEvent(event, config);
-        for (CarbonServerListener listener : config.getCarbonServerListeners()) {
+        config.getCarbonServerListeners().forEach(listener -> {
             if (logger.isLoggable(Level.FINE)) {
                 String eventName = (event == CarbonServerEvent.STARTING) ? "STARTING" : "STOPPING";
                 logger.log(Level.FINE, "Dispatching " + eventName + " event to " + listener.getClass().getName());
             }
             listener.notify(carbonServerEvent);
-        }
+        });
     }
 }
