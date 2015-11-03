8ad1ed4d8671000dd6e327249f93583f19dc9406
Manuri <amaya.uom@gmail.com>
2015-11-03 09:56:59 +0530

    CARBON-15609: Adding file existence check and endpoint availability check to SecurityVerificationTestCase

--- distribution/integration/tests-integration/tests/src/test/java/org/wso2/carbon/integration/tests/integration/SecurityVerificationTestCase.java
+++ distribution/integration/tests-integration/tests/src/test/java/org/wso2/carbon/integration/tests/integration/SecurityVerificationTestCase.java
@@ -30,6 +30,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.automation.engine.FrameworkConstants;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.test.utils.http.client.HttpRequestUtil;
+import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 import org.wso2.carbon.base.CarbonBaseUtils;
 import org.wso2.carbon.integration.tests.common.utils.CarbonIntegrationBaseTest;
 import org.wso2.carbon.utils.FileManipulator;
@@ -37,6 +39,10 @@ import org.wso2.carbon.utils.FileManipulator;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
 
 /**
  * A test case which verifies that all admin services deployed on this Carbon server are properly
@@ -65,6 +71,9 @@ public class SecurityVerificationTestCase extends CarbonIntegrationBaseTest {
                 new EndpointReference("http://" + automationContext.getInstance().getHosts().get("default") + ":" +
                         FrameworkConstants.SERVER_DEFAULT_HTTP_PORT
                         + "/services/SecurityVerifierService");
+
+        String address = epr.getAddress();
+        assert isWebAppDeployed("SecurityVerifierService", address) : address + "does not exist";
         opts.setTo(epr);
 
         client.setOptions(opts);
@@ -86,8 +95,10 @@ public class SecurityVerificationTestCase extends CarbonIntegrationBaseTest {
             return;
         }
         assert carbonHome != null : "carbonHome cannot be null";
-        File srcFile = new File(secVerifierDir + "SecVerifier.aar");
-        assert srcFile.exists() : srcFile.getAbsolutePath() + " does not exist";
+        //File srcFile = new File(secVerifierDir + "SecVerifier.aar");
+        Path srcFile = Paths.get(secVerifierDir, "SecVerifier.aar");
+       // assert srcFile.exists() : srcFile.getAbsolutePath() + " does not exist";
+        assert srcFile.toFile().exists() : srcFile.toFile().getAbsolutePath() + " does not exist";
 
         String deploymentPath = carbonHome + File.separator + "repository" + File.separator
                 + "deployment" + File.separator + "server" + File.separator + "axis2services";
@@ -95,9 +106,50 @@ public class SecurityVerificationTestCase extends CarbonIntegrationBaseTest {
         if (!depFile.exists() && !depFile.mkdir()) {
             throw new IOException("Error while creating the deployment folder : " + deploymentPath);
         }
-        File dstFile = new File(depFile.getAbsolutePath() + File.separator + "SecVerifier.aar");
-        log.info("Copying " + srcFile.getAbsolutePath() + " => " + dstFile.getAbsolutePath());
-        FileManipulator.copyFile(srcFile, dstFile);
-        Thread.sleep(20000);
+        //File dstFile = new File(depFile.getAbsolutePath() + File.separator + "SecVerifier.aar");
+        Path dstFile = Paths.get(depFile.getAbsolutePath(), "SecVerifier.aar");
+        //log.info("Copying " + srcFile.getAbsolutePath() + " => " + dstFile.getAbsolutePath());
+        log.info("Copying " + srcFile.toFile().getAbsolutePath() + " => " + dstFile.toFile().getAbsolutePath());
+        //FileManipulator.copyFile(srcFile, dstFile);
+        //Thread.sleep(20000);
+        FileManipulator.copyFile1(srcFile, dstFile);
+
+        assert isFileCopied(dstFile) : dstFile.toFile().getAbsolutePath() + "has not been copied";
+    }
+
+    private static boolean isFileCopied(Path path) {
+        Calendar startTime = Calendar.getInstance();
+        while (Calendar.getInstance().getTimeInMillis() - startTime.getTimeInMillis() < 90000)
+            ;
+
+        if (Files.exists(path)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isWebAppDeployed(String webAppName, String endpoint) {
+        log.info("waiting " + 90000 + " millis for webApp undeployment " + webAppName);
+        HttpResponse response;
+
+        Calendar startTime = Calendar.getInstance();
+        while ((Calendar.getInstance().getTimeInMillis() - startTime.getTimeInMillis()) < 90000) {
+            try {
+                response = HttpRequestUtil.sendGetRequest(endpoint, null);
+                if (response != null && !response.getData().isEmpty()) {
+                    return true;
+                }
+            } catch (IOException e) {
+                //Ignore IOExceptions
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) {
+
+            }
+
+        }
+        return false;
     }
 }
