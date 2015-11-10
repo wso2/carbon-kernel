57133f5271ee2651f03423fe50c118bda0cd5ed9
nipuni <nipuni880917@gmail.com>
2015-11-10 11:44:28 +0530

    adding @since header

--- launcher/src/main/java/org/wso2/carbon/launcher/ServerStatus.java
+++ launcher/src/main/java/org/wso2/carbon/launcher/ServerStatus.java
@@ -1,7 +1,9 @@
 package org.wso2.carbon.launcher;
 
 /**
- * Possible Carbon Server status
+ * Possible Carbon Server status.
+ *
+ * @since 5.0.0
  */
 public enum ServerStatus {
     STARTING, STARTED, STOPPING, STOPPED;
