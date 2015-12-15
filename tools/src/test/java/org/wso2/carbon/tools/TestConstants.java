c9e9217077a657c61515cf744f20ef437fff63b3
Sameera Jayasoma <sameera.madushan@gmail.com>
2015-12-15 21:49:51 +0530

    Updated a artifact id of a test artifact

--- tools/src/test/java/org/wso2/carbon/tools/TestConstants.java
+++ tools/src/test/java/org/wso2/carbon/tools/TestConstants.java
@@ -37,7 +37,7 @@ public class TestConstants {
     protected static final String IMPORT_PACKAGE = "DynamicImport-Package";
 
     protected static final Path SAMPLE_JAR_FILE = Paths
-                .get(TARGET_FOLDER, "test-resources", "tools-test-artifact.jar");
+                .get(TARGET_FOLDER, "test-resources", "tool-test-artifact.jar");
 
     /**
      * A constructor which prevents instantiating the TestConstants class.
