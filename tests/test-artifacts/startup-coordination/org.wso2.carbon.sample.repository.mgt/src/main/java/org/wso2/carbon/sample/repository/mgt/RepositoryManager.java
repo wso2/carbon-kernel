c58bd3ddf3ccb2a7ae8fa7ce4b2cd8c5a6fbebe4
Sameera Jayasoma <sameera.madushan@gmail.com>
2015-12-15 20:49:44 +0530

    Adding sample module to test multiple capabilities for a given required capability listener

---
+++ tests/test-artifacts/startup-coordination/org.wso2.carbon.sample.repository.mgt/src/main/java/org/wso2/carbon/sample/repository/mgt/RepositoryManager.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.carbon.sample.repository.mgt;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.sample.runtime.mgt.Runtime;
+import org.wso2.carbon.sample.transport.mgt.Transport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Sample Transport Manager Service Component class.
+ *
+ * @since 5.0.0
+ */
+@Component(
+        name = "org.wso2.carbon.sample.repository.mgt.RepositoryManager",
+        immediate = true
+)
+public class RepositoryManager {
+
+    private static List<Transport> transportList = new ArrayList<>();
+
+    private static List<Runtime> runtimeList = new ArrayList<>();
+
+    @Activate
+    public void activate(BundleContext bundleContext) {
+    }
+
+    @Deactivate
+    public void deactivate(BundleContext bundleContext) {
+
+    }
+
+    @Reference(
+            name = "sample.transport.service.reference",
+            service = Transport.class,
+            cardinality = ReferenceCardinality.MULTIPLE,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "deregisterTransport"
+    )
+    public void registerTransport(Transport transport) {
+        transportList.add(transport);
+    }
+
+    public void deregisterTransport(Transport transport) {
+        transportList.remove(transport);
+    }
+
+    public int getTransportCount() {
+        return transportList.size();
+    }
+
+    @Reference(
+            name = "sample.runtime.service.reference",
+            service = Runtime.class,
+            cardinality = ReferenceCardinality.MULTIPLE,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "deregisterRuntime"
+    )
+    public void registerRuntime(Runtime runtime) {
+        runtimeList.add(runtime);
+    }
+
+    public void deregisterRuntime(Runtime runtime) {
+        runtimeList.remove(runtime);
+    }
+
+    public int getRuntimeCount() {
+        return runtimeList.size();
+    }
+
+}
---
+++ tests/test-artifacts/startup-coordination/org.wso2.carbon.sample.repository.mgt/src/main/java/org/wso2/carbon/sample/repository/mgt/RepositoryServicesListener.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.carbon.sample.repository.mgt;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.kernel.startupresolver.RequiredCapabilityListener;
+
+
+/**
+ * Sample Transport Services Listener class.
+ *
+ * @since 5.0.0
+ */
+@Component(
+        name = "org.wso2.carbon.sample.repository.mgt.RepositoryServicesListener",
+        immediate = true,
+        service = RequiredCapabilityListener.class,
+        property = {
+                "capability-name=org.wso2.carbon.sample.transport.mgt.Transport," +
+                        "org.wso2.carbon.sample.runtime.mgt.Runtime",
+                "component-key=carbon-sample-repository-mgt"
+        }
+)
+public class RepositoryServicesListener implements RequiredCapabilityListener {
+    private static final Logger logger = LoggerFactory.getLogger(RepositoryServicesListener.class);
+    private BundleContext bundleContext;
+
+    @Override
+    public void onAllRequiredCapabilitiesAvailable() {
+        logger.info("All required services are available for : " + this.getClass().getName());
+        bundleContext.registerService(RepositoryManager.class, new RepositoryManager(), null);
+    }
+
+    @Activate
+    public void activate(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    @Deactivate
+    public void deactivate(BundleContext bundleContext) {
+
+    }
+}
