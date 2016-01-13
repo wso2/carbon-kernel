0bfd7d6fb4beca932b96d5f50e5020b11819f666
Kishanthan <kshanth2101@gmail.com>
2016-01-13 18:43:47 +0530

    adding support to store JAAS Subject within carbon context

--- modules/carbon-multitenancy-api/org.wso2.carbon.context.api/src/main/java/org/wso2/carbon/context/api/internal/CarbonContextHolder.java
+++ modules/carbon-multitenancy-api/org.wso2.carbon.context.api/src/main/java/org/wso2/carbon/context/api/internal/CarbonContextHolder.java
@@ -29,7 +29,7 @@ public class CarbonContextHolder {
         }
     };
 
-    public CarbonContextHolder() {
+    private CarbonContextHolder() {
         tenantDomain = null;
         subject = null;
     }
