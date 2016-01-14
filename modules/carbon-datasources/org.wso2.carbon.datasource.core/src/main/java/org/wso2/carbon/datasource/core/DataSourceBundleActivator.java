46cda99229d190f67751c0cd763d4888abe051ff
dinushab <dinushaboteju@gmail.com>
2016-01-14 15:41:09 +0530

    Added license header to DataSourceBundleActivator.java

--- modules/carbon-datasources/org.wso2.carbon.datasource.core/src/main/java/org/wso2/carbon/datasource/core/DataSourceBundleActivator.java
+++ modules/carbon-datasources/org.wso2.carbon.datasource.core/src/main/java/org/wso2/carbon/datasource/core/DataSourceBundleActivator.java
@@ -1,17 +1,24 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.wso2.carbon.datasource.core;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.wso2.carbon.datasource.common.DataSourceException;
-import org.wso2.carbon.datasource.core.DataSourceManager;
-
-import java.util.Hashtable;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 public class DataSourceBundleActivator implements BundleActivator {
 
@@ -19,8 +26,9 @@ public class DataSourceBundleActivator implements BundleActivator {
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        log.info("Activating data source bundle");
+        log.info("initializing data source bundle");
         DataSourceManager.getInstance().initSystemDataSources();
+        log.info("initializing data source bundle completed");
     }
 
 
