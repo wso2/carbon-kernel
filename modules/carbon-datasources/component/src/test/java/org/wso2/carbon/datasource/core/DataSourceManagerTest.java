ca29970d248053dee7ffe3f024168dd744607f6d
dinushab <dinushaboteju@gmail.com>
2016-02-08 10:03:49 +0530

    Added JNDI custom classes for unit tests.

--- modules/carbon-datasources/component/src/test/java/org/wso2/carbon/datasource/core/DataSourceManagerTest.java
+++ modules/carbon-datasources/component/src/test/java/org/wso2/carbon/datasource/core/DataSourceManagerTest.java
@@ -15,25 +15,15 @@
  */
 package org.wso2.carbon.datasource.core;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.wso2.carbon.datasource.core.common.DataSourceException;
-import org.wso2.carbon.kernel.config.model.CarbonConfiguration;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 /**
  * Test case for DataSourceManager class.
@@ -55,39 +45,17 @@ public class DataSourceManagerTest {
         Path carbonHomePath = Paths.get("component", "target", "carbonHome");
         System.setProperty("carbon.home", carbonHomePath.toFile().getAbsolutePath());
 
-        Path configFilePath = Paths.get("component", "src", "test", "resources", "conf", "datasources", "master-datasources.xml");
+//        Path configFilePath = Paths.get("component", "src", "test", "resources", "conf", "datasources", "master-datasources.xml");
+        Path configFilePath = Paths.get("src", "test", "resources", "conf", "datasources", "master-datasources.xml");
         Path configPathCopyLocation = Paths.get("component/target/carbonHome/conf/datasources/master-datasources.xml");
         Utils.copy(configFilePath.toFile().getAbsolutePath(), configPathCopyLocation.toFile().getAbsolutePath());
 
-//        OutputStream outputStream = null;
-//        try (InputStream inputStream = new FileInputStream(configFileLocation)) {
-//
-//            String yamlFileString;
-//            try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
-//                yamlFileString = scanner.useDelimiter("\\A").next();
-//                yamlFileString = org.wso2.carbon.kernel.utils.Utils.substituteVariables(yamlFileString);
-//            }
-//            File confPath = new File("component/target/carbonHome/conf/datasources");
-//            confPath.mkdirs();
-//            outputStream = new FileOutputStream(confPath + "/master-datasources.xml");
-//            outputStream.write(yamlFileString.getBytes());
-//        } catch (IOException e) {
-//            String errorMessage = "Failed populate CarbonConfiguration from " + configFileLocation;
-//            throw new RuntimeException(errorMessage);
-//        } finally {
-//            if(outputStream != null) {
-//                try {
-//                    outputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-
     }
 
-
-    private void loadDataSource() {
+    @Test
+    private void checkDsRepoTest() {
+        DataSourceRepository dsRepo = dsManager.getDataSourceRepository();
+        Assert.assertNotNull(dsRepo, "Expected a DataSourceRepository, but found none!!!");
         //Does loading the data prior to run the test.
     }
 
---
+++ modules/carbon-datasources/component/src/test/java/org/wso2/carbon/datasource/core/jndi/CustomContext.java
@@ -0,0 +1,188 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.carbon.datasource.core.jndi;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import javax.naming.Binding;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NameClassPair;
+import javax.naming.NameParser;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+
+public class CustomContext implements Context{
+
+    private HashMap<String, Object> contextObjects;
+    private Hashtable<?, ?> environment;
+
+    public CustomContext(Hashtable<?, ?> environment) {
+        this.contextObjects = new HashMap();
+        this.environment = environment;
+    }
+
+    @Override
+    public Object lookup(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Object lookup(String name) throws NamingException {
+        Object obj = contextObjects.get(name);
+        if(obj == null) {
+            throw new NamingException("No binding found for the name: " + name);
+        }
+        return obj;
+    }
+
+    @Override
+    public void bind(Name name, Object obj) throws NamingException {
+
+    }
+
+    @Override
+    public void bind(String name, Object obj) throws NamingException {
+        contextObjects.put(name, obj);
+    }
+
+    @Override
+    public void rebind(Name name, Object obj) throws NamingException {
+
+    }
+
+    @Override
+    public void rebind(String name, Object obj) throws NamingException {
+        contextObjects.put(name, obj);
+    }
+
+    @Override
+    public void unbind(Name name) throws NamingException {
+
+    }
+
+    @Override
+    public void unbind(String name) throws NamingException {
+        contextObjects.remove(name);
+    }
+
+    @Override
+    public void rename(Name oldName, Name newName) throws NamingException {
+
+    }
+
+    @Override
+    public void rename(String oldName, String newName) throws NamingException {
+
+    }
+
+    @Override
+    public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public void destroySubcontext(Name name) throws NamingException {
+
+    }
+
+    @Override
+    public void destroySubcontext(String name) throws NamingException {
+
+    }
+
+    @Override
+    public Context createSubcontext(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Context createSubcontext(String name) throws NamingException {
+        CustomContext subContext = new CustomContext(environment);
+        contextObjects.put(name, subContext);
+        return subContext;
+    }
+
+    @Override
+    public Object lookupLink(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Object lookupLink(String name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public NameParser getNameParser(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public NameParser getNameParser(String name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Name composeName(Name name, Name prefix) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public String composeName(String name, String prefix) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Object addToEnvironment(String propName, Object propVal) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Object removeFromEnvironment(String propName) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public Hashtable<?, ?> getEnvironment() throws NamingException {
+        return null;
+    }
+
+    @Override
+    public void close() throws NamingException {
+
+    }
+
+    @Override
+    public String getNameInNamespace() throws NamingException {
+        return null;
+    }
+}
