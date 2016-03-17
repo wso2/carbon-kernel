333c9740f4c182b54031ebeaaf34f9334ccee28a
Niranjan-K <niranjan.karu@gmail.com>
2016-03-17 21:35:54 +0530

    revert pax logging version to 1.8.4

--- launcher/src/test/java/org/wso2/carbon/launcher/test/logging/handlers/CommonsLogHandler.java
+++ launcher/src/test/java/org/wso2/carbon/launcher/test/logging/handlers/CommonsLogHandler.java
@@ -122,4 +122,9 @@ public class CommonsLogHandler implements Log {
     public void fatal(Object o, Throwable throwable) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
+
+    @Override
+    public int getLogLevel() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
