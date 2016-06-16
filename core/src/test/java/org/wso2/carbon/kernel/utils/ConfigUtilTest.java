c83b8d1f423c50776f55f79769445c4a0a08ca26
shan1024 <gambit1024@gmail.com>
2016-06-16 17:46:00 +0530

    New tests added

--- core/src/test/java/org/wso2/carbon/kernel/utils/ConfigUtilTest.java
+++ core/src/test/java/org/wso2/carbon/kernel/utils/ConfigUtilTest.java
@@ -70,7 +70,7 @@ public class ConfigUtilTest {
         setUpEnvironment();
         try {
             Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example.xml");
-            File file = new File(resourcePath.toString());
+            File file = resourcePath.toFile();
 
             JAXBContext jaxbContext = JAXBContext.newInstance(Configurations.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
@@ -125,7 +125,7 @@ public class ConfigUtilTest {
         setUpEnvironment();
         try {
             Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example.yml");
-            File file = new File(resourcePath.toString());
+            File file = resourcePath.toFile();
             FileInputStream fileInputStream = new FileInputStream(file);
             Yaml yaml = new Yaml();
             Map map = yaml.loadAs(fileInputStream, Map.class);
@@ -184,7 +184,7 @@ public class ConfigUtilTest {
         setUpEnvironment();
         try {
             Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example.properties");
-            File file = new File(resourcePath.toString());
+            File file = resourcePath.toFile();
             FileInputStream fileInputStream = new FileInputStream(file);
             java.util.Properties properties = new java.util.Properties();
             properties.load(fileInputStream);
@@ -224,14 +224,14 @@ public class ConfigUtilTest {
     }
 
     @Test
-    public void getConfigTest() {
+    public void getConfigTest1() {
 
         setUpEnvironment();
 
         String newValue = ConfigUtil.getConfig("[Example.xml]/configurations/tenant");
         Assert.assertEquals(newValue, "new_tenant");
         newValue = ConfigUtil.getConfig("[Example.xml]/configurations/transports/transport/port");
-        Assert.assertEquals(newValue, "$sys:abc.http.port");
+        Assert.assertEquals(newValue, "8001");
         newValue = ConfigUtil.getConfig("[Example.xml]/configurations/transports/transport[name='abc']/@secure");
         Assert.assertEquals(newValue, "true");
         newValue = ConfigUtil.getConfig("[Example.xml]/configurations/transports/transport[name='pqr']/port");
@@ -242,7 +242,7 @@ public class ConfigUtilTest {
         newValue = ConfigUtil.getConfig("[Example.yml]/tenant");
         Assert.assertEquals(newValue, "new_tenant");
         newValue = ConfigUtil.getConfig("[Example.yml]/transports/transport/port");
-        Assert.assertEquals(newValue, "$sys:abc.http.port");
+        Assert.assertEquals(newValue, "8001");
         newValue = ConfigUtil.getConfig("[Example.yml]/transports/transport[name='abc']/secure");
         Assert.assertEquals(newValue, "true");
         newValue = ConfigUtil.getConfig("[Example.yml]/transports/transport[name='pqr']/port");
@@ -253,9 +253,9 @@ public class ConfigUtilTest {
         newValue = ConfigUtil.getConfig("[Example.properties]/tenant");
         Assert.assertEquals(newValue, "new_tenant");
         newValue = ConfigUtil.getConfig("[Example.properties]/transport.abc.port");
-        Assert.assertEquals(newValue, "$sys:abc.http.port");
+        Assert.assertEquals(newValue, "8001");
         newValue = ConfigUtil.getConfig("[Example.properties]/transport.abc.secure");
-        Assert.assertEquals(newValue, "$sys:abc.port.secure,true");
+        Assert.assertEquals(newValue, "true");
         newValue = ConfigUtil.getConfig("[Example.properties]/transport.pqr.port");
         Assert.assertEquals(newValue, null);
         newValue = ConfigUtil.getConfig("[Example.properties]/transport.pqr.secure");
@@ -263,10 +263,25 @@ public class ConfigUtilTest {
     }
 
     @Test(expectedExceptions = RuntimeException.class)
+    public void getConfigTest2() {
+        setCarbonHome();
+        System.clearProperty("abc.http.port");
+        String newValue = ConfigUtil.getConfig("[Example.xml]/configurations/transports/transport/port");
+        Assert.assertEquals(newValue, "$sys:abc.http.port");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void getConfigTest3() {
+        setCarbonHome();
+        String newValue = ConfigUtil.getConfig("[Example.xml]/configurations/transports/transport/port");
+        Assert.assertEquals(newValue, "$env:abc.http.port");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
     public void invalidConfigTest() {
         setUpEnvironment();
         Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example2.xml");
-        File file = new File(resourcePath.toString());
+        File file = resourcePath.toFile();
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             XML configXml = ConfigUtil.getConfig(fileInputStream, "Example.xml", XML.class);
@@ -277,18 +292,96 @@ public class ConfigUtilTest {
         }
     }
 
-    private void setUpEnvironment() {
+    @Test
+    public void invalidFileTest() {
+        setUpEnvironment();
+        Path resourcePath = Paths.get(basedir, "Example3.xml");
+        File file = resourcePath.toFile();
+        XML configXml = ConfigUtil.getConfig(file, XML.class);
+        Assert.assertNull(configXml);
+    }
+
+    @Test
+    public void invalidFileNameTest() {
+        setUpEnvironment();
+        String tenant = ConfigUtil.getConfig("[Example2.xml]/configurations/tenant/name");
+        Assert.assertNull(tenant);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void invalidSysPropTest() {
+        setUpEnvironment();
+        Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example3.xml");
+        File file = resourcePath.toFile();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            XML configs = ConfigUtil.getConfig(fileInputStream, "Example3.xml", XML.class);
+            Assert.assertNotNull(configs.getValue());
+        } catch (FileNotFoundException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void invalidEnvVarTest() {
+        setUpEnvironment();
+        Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example3.xml");
+        File file = resourcePath.toFile();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            XML configs = ConfigUtil.getConfig(fileInputStream, "Example4.xml", XML.class);
+            Assert.assertNotNull(configs.getValue());
+        } catch (FileNotFoundException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void validEnvVarWithDefaultTest() {
+        setUpEnvironment();
+        Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example3.xml");
+        File file = resourcePath.toFile();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            XML configs = ConfigUtil.getConfig(fileInputStream, "Example5.xml", XML.class);
+            Assert.assertNotNull(configs.getValue());
+        } catch (FileNotFoundException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void validSysPropWithDefaultTest() {
+        setUpEnvironment();
+        Path resourcePath = Paths.get(basedir, "src", "test", "resources", "configutil", "Example3.xml");
+        File file = resourcePath.toFile();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            XML configs = ConfigUtil.getConfig(fileInputStream, "Example6.xml", XML.class);
+            Assert.assertNotNull(configs.getValue());
+        } catch (FileNotFoundException e) {
+            Assert.fail();
+        }
+    }
+
+    private void setCarbonHome() {
         Path carbonHome = Paths.get("");
         carbonHome = Paths.get(carbonHome.toString(), "src", "test", "resources");
         System.setProperty(Constants.CARBON_HOME, carbonHome.toString());
         logger.debug("Carbon Home Absolute path set to: " + carbonHome.toAbsolutePath());
+    }
 
+    private void setUpEnvironment() {
+
+        setCarbonHome();
         //This is how to set Environment Variables
         Map<String, String> envVarMap = new HashMap<>();
         envVarMap.put("pqr.http.port", "8501");
+        envVarMap.put("sample.abc.port", "8081");
         setEnv(envVarMap);
         //This is how to set System properties
         System.setProperty("abc.http.port", "8001");
+        System.setProperty("sample.xyz.port", "9091");
         System.setProperty("pqr.secure", "true");
     }
 
