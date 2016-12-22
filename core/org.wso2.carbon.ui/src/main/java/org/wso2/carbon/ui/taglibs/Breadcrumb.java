e0f7e509ff3cd5c2e48f3943a96a7a34c8a19a63
shan1024 <gambit1024@gmail.com>
2016-12-22 19:29:49 +0530

    Fix XSS in viewflows

--- core/org.wso2.carbon.ui/src/main/java/org/wso2/carbon/ui/taglibs/Breadcrumb.java
+++ core/org.wso2.carbon.ui/src/main/java/org/wso2/carbon/ui/taglibs/Breadcrumb.java
@@ -19,7 +19,6 @@ package org.wso2.carbon.ui.taglibs;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.owasp.encoder.Encode;
 import org.wso2.carbon.ui.BreadCrumbGenerator;
 import org.wso2.carbon.ui.CarbonUIUtil;
 import org.wso2.carbon.ui.deployment.beans.BreadCrumbItem;
@@ -29,6 +28,8 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.util.ArrayList;
@@ -227,8 +228,13 @@ public class Breadcrumb extends BodyTagSupport {
 		}
 
         content.append("<script type=\"text/javascript\">\n");
-        content.append("    setCookie('current-breadcrumb', '" + Encode.forJavaScriptAttribute(cookieContent) + "');\n");
-        content.append("    document.onload=setBreadcrumDiv();\n");
+		try {
+			content.append("    setCookie('current-breadcrumb', '" + URLEncoder.encode(cookieContent, "UTF-8")
+					+ "');\n");
+		} catch (UnsupportedEncodingException e) {
+			log.error("Unsupported encoding UTF-8", e);
+		}
+		content.append("    document.onload=setBreadcrumDiv();\n");
 		content.append("    function setBreadcrumDiv () {\n");
 		content.append("        var breadcrumbDiv = document.getElementById('breadcrumb-div');\n");
 		if(! hidden){
