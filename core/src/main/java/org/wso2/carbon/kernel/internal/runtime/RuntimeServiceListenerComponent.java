51e90881a3112fdbc9c541f7f2b42ad8b0b211ba
Jayanga Dissanayake <jsdjayanga@gmail.com>
2017-04-05 14:51:26 +0530

    Fix merge conflicts

--- core/src/main/java/org/wso2/carbon/kernel/internal/runtime/RuntimeServiceListenerComponent.java
+++ core/src/main/java/org/wso2/carbon/kernel/internal/runtime/RuntimeServiceListenerComponent.java
@@ -29,7 +29,6 @@ import org.wso2.carbon.kernel.runtime.Runtime;
 import org.wso2.carbon.kernel.runtime.RuntimeService;
 import org.wso2.carbon.kernel.startupresolver.RequiredCapabilityListener;
 import org.wso2.carbon.kernel.startupresolver.StartupServiceUtils;
-import org.wso2.carbon.utils.MBeanRegistrator;
 
 /**
  * This service  component is responsible for retrieving the Runtime OSGi service and register each runtime
