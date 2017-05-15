3e0f34aa7a8216c46408f97af637eb2dfe443f5c
daneshk <dknkuruppu@gmail.com>
2017-05-15 19:12:11 +0530

    Remove Bootup Validation logic

--- core/org.wso2.carbon.core.bootup.validator/src/main/java/org/wso2/carbon/core/bootup/validator/internal/BootupValidationActivator.java
+++ core/org.wso2.carbon.core.bootup.validator/src/main/java/org/wso2/carbon/core/bootup/validator/internal/BootupValidationActivator.java
@@ -17,59 +17,14 @@
  */
 package org.wso2.carbon.core.bootup.validator.internal;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import org.wso2.carbon.core.bootup.validator.ConfigurationValidator;
-import org.wso2.carbon.core.bootup.validator.util.ConfigValidationXMLProcessor;
-import org.wso2.carbon.core.bootup.validator.util.ValidationResult;
-import org.wso2.carbon.core.bootup.validator.util.ValidationResultPrinter;
-import org.wso2.carbon.utils.CarbonUtils;
-
+@Deprecated
 public class BootupValidationActivator implements BundleActivator {
 
-	private static final Log log = LogFactory.getLog(BootupValidationActivator.class);
-	private ConfigValidationXMLProcessor configXMLProcessor;
-	private List<ConfigurationValidator> validators;
-	private Map<String, ValidationResult> allResults = new HashMap<String, ValidationResult>();
-
 	public void start(BundleContext bundleContext) {
-		try {
-            // If Carbon Kernel is running in the optimized mode, we avoid validating the resources
-            if (CarbonUtils.isOptimized()) {
-                return;
-            }
-			// parsing the config_validation.xml
-			configXMLProcessor = new ConfigValidationXMLProcessor();
-			configXMLProcessor.parseConfigValidationXml();
-			if (ConfigValidationXMLProcessor.isActivated()) {
-				log.debug("Bootup Validator is activated...");
-				validators = configXMLProcessor.getValidators();
-				validateConfigurations(validators);
-				printWarnings();
-			} else {
-				log.debug("Bootup Validator is disabled...");
-			}
-		} catch (Exception e) {
-			log.error("Error occured while starting Bootup Validation Activator", e);
-		}
-	}
 
-	private void validateConfigurations(List<ConfigurationValidator> configValidators) {
-		for (ConfigurationValidator validator : configValidators) {
-			Map<String, ValidationResult> results = validator.validate();
-			allResults.putAll(results);
-		}
-	}
-
-	private void printWarnings() {
-		ValidationResultPrinter.logConsoleWarnings(allResults);
 	}
 
 	public void stop(BundleContext arg0) throws Exception {
