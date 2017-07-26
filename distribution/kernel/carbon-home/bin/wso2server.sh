4c09d989e763eff10f6bc12261c2030da6f512a1
cnapagoda <cnapagoda@gmail.com>
2017-07-26 14:52:19 +0530

    Update server script with new system property

--- distribution/kernel/carbon-home/bin/wso2server.sh
+++ distribution/kernel/carbon-home/bin/wso2server.sh
@@ -313,7 +313,7 @@ do
     -Djava.net.preferIPv4Stack=true \
     -Dcom.ibm.cacheLocalHost=true \
     -DworkerNode=false \
-    -Dorg.wso2.ignoreHostnameVerification=true \
+    -Dhttpclient.hostnameVerifier="DefaultAndLocalhost" \
     org.wso2.carbon.bootstrap.Bootstrap $*
     status=$?
 done
