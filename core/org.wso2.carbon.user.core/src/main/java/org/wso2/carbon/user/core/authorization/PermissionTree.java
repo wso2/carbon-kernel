3f2be438be117a548cc54d2bb1fccdc8c7bcfad7
harsha89 <harsz89@gmail.com>
2017-09-15 12:17:00 +0530

    resolved conflicts

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/authorization/PermissionTree.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/authorization/PermissionTree.java
@@ -27,6 +27,7 @@ import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.internal.UserStoreMgtDSComponent;
 import org.wso2.carbon.user.core.util.DatabaseUtil;
 import org.wso2.carbon.user.core.util.UserCoreUtil;
+import org.wso2.carbon.utils.xml.StringUtils;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
@@ -960,6 +961,17 @@ public class PermissionTree {
      * @throws org.wso2.carbon.user.core.UserStoreException throws if fail to update permission tree from DB
      */
     void updatePermissionTree() throws UserStoreException {
+        updatePermissionTree("");
+    }
+
+    /**
+     * Update permission tree from database for a given resource id if permission tree is already cached.
+     * If permission tree isn't cached, then this method will load full permission tree.
+     *
+     * @param resourceId registry resource path
+     * @throws org.wso2.carbon.user.core.UserStoreException throws if fail to update permission tree from DB
+     */
+    void updatePermissionTree(String resourceId) throws UserStoreException {
         Cache<PermissionTreeCacheKey, GhostResource<TreeNode>> permissionCache = this.getPermissionTreeCache();
         if (permissionCache != null) {
             PermissionTreeCacheKey cacheKey = new PermissionTreeCacheKey(cacheIdentifier, tenantId);
@@ -980,6 +992,11 @@ public class PermissionTree {
                                 log.debug("Set resource to true");
                             }
                         }
+                    }
+                } else {
+                    if(!StringUtils.isEmpty(resourceId)) {
+                        //If permission tree is cached, only update the permissions of given resource path
+                        updateResourcePermissionsById(resourceId);
                     }
                 }
             } else {
@@ -1057,6 +1074,48 @@ public class PermissionTree {
     }
     /**
      * update permission tree from cache
+     *
+     * @throws org.wso2.carbon.user.core.UserStoreException throws if fail to update permission tree from DB
+     */
+    void updateResourcePermissionsById(String resourceId) throws UserStoreException {
+        Connection dbConnection = null;
+        ResultSet rs = null;
+        PreparedStatement statement = null;
+        try {
+            PermissionTree tree = new PermissionTree();
+            tree.root = this.root;
+            dbConnection = getDBConnection();
+            // Populating role permissions
+            statement = dbConnection.prepareStatement(DBConstants.GET_EXISTING_ROLE_PERMISSIONS_BY_RESOURCE_ID);
+            statement.setInt(1, tenantId);
+            statement.setInt(2, tenantId);
+            statement.setString(3, resourceId);
+            rs = statement.executeQuery();
+            write.lock();
+            try {
+                while (rs.next()) {
+                    short allow = rs.getShort(3);
+                    String roleName = rs.getString(1);
+                    String domain = rs.getString(5);
+                    String roleWithDomain = UserCoreUtil.addDomainToName(roleName, domain);
+                    if (allow == UserCoreConstants.ALLOW) {
+                        tree.authorizeRoleInTree(roleWithDomain, rs.getString(2), rs.getString(4), false);
+                    }
+                }
+            } finally {
+                write.unlock();
+            }
+        } catch (SQLException e) {
+            throw new UserStoreException(
+                    "Error loading authorizations. Please check the database. Error message is "
+                            + e.getMessage(), e);
+        } finally {
+            DatabaseUtil.closeAllConnections(dbConnection, rs, statement);
+        }
+    }
+
+    /**
+     * Update permission tree from cache.
      *
      * @throws org.wso2.carbon.user.core.UserStoreException throws if fail to update permission tree from DB
      */
