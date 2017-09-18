aa1a50640d5305ff35113935b244f54203c7622c
thanujalk <jatljayasinghe@gmail.com>
2017-09-18 18:29:51 +0530

    Allow using database tuning parameters in secondary user-stores

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCRealmConstants.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCRealmConstants.java
@@ -145,6 +145,39 @@ public final class JDBCRealmConstants {
     public static final String TIME_BETWEEN_EVICTION_RUNS_MILLIS = "timeBetweenEvictionRunsMillis";
     public static final String MIN_EVIC_TABLE_IDLE_TIME_MILLIS = "minEvictableIdleTimeMillis";
     public static final String NUM_TESTS_PEREVICTION_RUN = "numTestsPerEvictionRun";
+    public static final String DEFAULT_AUTO_COMMIT = "defaultAutoCommit";
+    public static final String DEFAULT_READ_ONLY = "defaultReadOnly";
+    public static final String DEFAULT_TRANSACTION_ISOLATION="defaultTransactionIsolation";
+    public static final String DEFAULT_CATALOG = "defaultCatalog";
+    public static final String INITIAL_SIZE = "initialSize";
+    public static final String TEST_ON_RETURN = "testOnReturn";
+    public static final String TEST_ON_BORROW = "testOnBorrow";
+    public static final String VALIDATOR_CLASS_NAME = "validatorClassName";
+    public static final String NUM_TESTS_PER_EVICTION_RUN = "numTestsPerEvictionRun";
+    public static final String ACCESS_TO_UNDERLYING_CONNECTION_ALLOWED = "accessToUnderlyingConnectionAllowed";
+    public static final String REMOVE_ABANDONED = "removeAbandoned";
+    public static final String REMOVE_ABANDONED_TIMEOUT = "removeAbandonedTimeout";
+    public static final String LOG_ABANDONED = "logAbandoned";
+    public static final String CONNECTION_PROPERTIES = "connectionProperties";
+    public static final String INIT_SQL = "initSQL";
+    public static final String JDBC_INTERCEPTORS = "jdbcInterceptors";
+    public static final String JMX_ENABLED = "jmxEnabled";
+    public static final String FAIR_QUEUE = "fairQueue";
+    public static final String ABANDON_WHEN_PERCENTAGE_FULL = "abandonWhenPercentageFull";
+    public static final String MAX_AGE = "maxAge";
+    public static final String USE_EQUALS = "useEquals";
+    public static final String SUSPECT_TIMEOUT = "suspectTimeout";
+    public static final String VALIDATION_QUERY_TIMEOUT = "validationQueryTimeout";
+    public static final String ALTERNATE_USERNAME_ALLOWED = "alternateUsernameAllowed";
+
+    public static final class TX_ISOLATION_LEVELS {
+        public static final String NONE = "NONE";
+        public static final String READ_COMMITTED = "READ_COMMITTED";
+        public static final String READ_UNCOMMITTED = "READ_UNCOMMITTED";
+        public static final String REPEATABLE_READ = "REPEATABLE_READ";
+        public static final String SERIALIZABLE = "SERIALIZABLE";
+    }
+
     // mssql
     public static final String ADD_USER_TO_ROLE_MSSQL_SQL = "INSERT INTO UM_USER_ROLE (UM_USER_ID, UM_ROLE_ID, UM_TENANT_ID) SELECT (SELECT UM_ID FROM UM_USER WHERE UM_USER_NAME=? AND UM_TENANT_ID=?),(SELECT UM_ID FROM UM_ROLE WHERE UM_ROLE_NAME=? AND UM_TENANT_ID=?),(?)";
     public static final String ADD_ROLE_TO_USER_MSSQL_SQL = "INSERT INTO UM_USER_ROLE (UM_ROLE_ID, UM_USER_ID, UM_TENANT_ID) SELECT (SELECT UM_ID FROM UM_ROLE WHERE UM_ROLE_NAME=? AND UM_TENANT_ID=?),(SELECT UM_ID FROM UM_USER WHERE UM_USER_NAME=? AND UM_TENANT_ID=?), (?)";
