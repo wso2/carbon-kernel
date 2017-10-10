347a0b2b3289b43f64331f5bb2e73004a24be02e
Kishanthan <kshanth2101@gmail.com>
2017-10-10 17:40:01 +0530

    Add test cases for Axis2Module and Axis2Service registries

---
+++ core/org.wso2.carbon.utils/src/test/java/org/wso2/carbon/utils/deployment/DummyHandler.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.wso2.carbon.utils.deployment;
+
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.engine.Handler;
+import org.apache.axis2.handlers.AbstractHandler;
+
+/**
+ * Dummy axis2 handler used with Axis2ModuleRegistry.
+ */
+public class DummyHandler extends AbstractHandler{
+    public Handler.InvocationResponse invoke(MessageContext msgContext) throws AxisFault {
+        return Handler.InvocationResponse.CONTINUE;
+    }
+}
