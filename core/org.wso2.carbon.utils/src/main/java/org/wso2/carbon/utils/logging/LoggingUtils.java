956f4569f3d2b603e7e013f350a594355c1b1b6a
Menaka Jayawardena <menaka@wso2.com>
2018-09-06 12:40:23 +0530

    Restored license header.

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/logging/LoggingUtils.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/logging/LoggingUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 WSO2, Inc. http://www.wso2.org
+ * Copyright 2005,2014 WSO2, Inc. http://www.wso2.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
