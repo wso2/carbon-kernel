0b20ff95036d108172fedb7972252ae1b8e24b1d
Tharindu Dharmarathna <tharindua@wso2.com>
2019-02-07 14:07:10 +0530

    Add km

--- config-parser/src/main/java/org/wso2/ei/config/ValueInferrer.java
+++ config-parser/src/main/java/org/wso2/ei/config/ValueInferrer.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
