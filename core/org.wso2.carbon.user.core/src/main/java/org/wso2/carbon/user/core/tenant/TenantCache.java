5974bb0a5e932386bcf1f880e7cb080cc241be64
Kavindu-dodan <kavindudodanduwa@gmail.com>
2019-02-08 16:25:00 +0530

    Adding license header and removing unused import

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/TenantCache.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/TenantCache.java
@@ -6,15 +6,14 @@
  *  in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.wso2.carbon.user.core.tenant;
 
@@ -83,7 +82,7 @@ public class TenantCache {
                 cache.put(key, entry);
                 if (log.isDebugEnabled()) {
                     log.debug(TENANT_CACHE + " which is under " + TENANT_CACHE_MANAGER + ", added the entry : " + entry
-                        + " for the key : " + key + " successfully");
+                            + " for the key : " + key + " successfully");
                 }
             } else {
                 if (log.isDebugEnabled()) {
