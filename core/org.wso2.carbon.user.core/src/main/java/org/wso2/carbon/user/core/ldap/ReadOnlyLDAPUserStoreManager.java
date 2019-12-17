f489d598b3098fa14a5d2928d60255c87e1ea2cd
somindatommy <sominda@wso2.com>
2019-12-17 09:55:18 +0530

    Fix comments

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/ReadOnlyLDAPUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/ReadOnlyLDAPUserStoreManager.java
@@ -2995,7 +2995,8 @@ public class ReadOnlyLDAPUserStoreManager extends AbstractUserStoreManager {
     }
 
     /**
-     * Method to verify whether the filter operation is a single attribute filter scenario or multi attribute filter scenario
+     * Method to verify whether the filter operation is a single attribute filter scenario or multi attribute
+     * filter scenario.
      *
      * @param expressionConditions Expression conditions
      * @return True if the operation is a single attribute filter.
