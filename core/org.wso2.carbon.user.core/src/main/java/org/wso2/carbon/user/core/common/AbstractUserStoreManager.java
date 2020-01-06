eb9cb79701b8ab64f4b80dda7018233966f23ec9
Gayashan Bombuwala <gayashanbc@gmail.com>
2020-01-06 10:11:04 +0530

    Refactor getRoleListOfUserFromDatabase() adhering to new userId implementation (#2512)
    * Refactor getRoleListOfUserFromDatabase() adhering to new userId implementation
    
    * Fix a typo

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
@@ -7953,7 +7953,16 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
             return (String[]) object;
         }
 
-        return getUserRoles(username, filter);
+        if (isUniqueUserIdEnabledInUserStore(getUserStore(username))) {
+            String userID = getUserIDFromUserName(username);
+            if (userID == null) {
+                // According to implementation, getRoleListOfUser method would return everyone role name for all users.
+                return new String[]{realmConfig.getEveryOneRoleName()};
+            }
+            return getUserRolesWithID(userID, filter).toArray(new String[0]);
+        } else {
+            return getUserRoles(username, filter);
+        }
     }
 
 
