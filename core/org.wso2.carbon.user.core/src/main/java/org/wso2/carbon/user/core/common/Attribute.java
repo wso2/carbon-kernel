dd8a81a027aabf8f40c9e78a1c465c505b514b53
GDRDABARERA <gdrdabarera@gmail.com>
2020-02-10 11:24:49 +0530

    Added Attribute class for further clarity

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/Attribute.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/Attribute.java
@@ -1,5 +1,27 @@
 package org.wso2.carbon.user.core.common;
 
-public class Attribute {
+public class Attribute <T> {
 
+    private String key;
+    private T value;
+
+    public String getKey() {
+
+        return key;
+    }
+
+    public void setKey(String key) {
+
+        this.key = key;
+    }
+
+    public T getValue() {
+
+        return value;
+    }
+
+    public void setValue(T value) {
+
+        this.value = value;
+    }
 }
