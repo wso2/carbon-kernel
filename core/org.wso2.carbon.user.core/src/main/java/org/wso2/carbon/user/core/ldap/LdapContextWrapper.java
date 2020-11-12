27e2946e056d283d43641921bb1fd6f1c2a006cd
Dilan Tharaka <tharakamd.12@cse.mrt.ac.lk>
2020-11-12 13:24:25 +0530

    Fix LDAP TLS authentication issue

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/LdapContextWrapper.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/LdapContextWrapper.java
@@ -153,6 +153,8 @@ public class LdapContextWrapper implements LdapContext {
                         "TLS LdapContext environment");
             }
         }
+
+        ldapContext.getAttributes("");
     }
 
     @Override
