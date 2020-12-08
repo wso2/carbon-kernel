93e303f9663e028381be7aef1ca0ffdbfe39f69e
Isura <isuradilhara@gmail.com>
2020-12-08 15:35:11 +0530

    Fix https://github.com/wso2/product-is/issues/9954 (#2860)

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
@@ -2780,7 +2780,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
         }
 
         UserStoreManager userManager = this;
-        if (StringUtils.isNotEmpty(extractedDomain)) {
+        if (StringUtils.isNotEmpty(extractedDomain) && !StringUtils.equalsIgnoreCase(getMyDomainName(), extractedDomain)) {
             UserStoreManager secondaryUserStoreManager = getSecondaryUserStoreManager(extractedDomain);
             if (secondaryUserStoreManager != null) {
                 userManager = secondaryUserStoreManager;
@@ -2788,6 +2788,8 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
                     log.debug("Domain: " + extractedDomain + " is passed with the claim and user store manager is loaded"
                             + " for the given domain name.");
                 }
+            } else {
+                throw new UserStoreClientException("Invalid Domain Name: " + extractedDomain);
             }
         }
 
@@ -3013,7 +3015,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
         * This method("getUserListWithID") can be called for secondary userstore managers.
         * At that time the "extractedDomain" is the name of the "this" usertore manager.
         */
-        if (StringUtils.isNotEmpty(extractedDomain) && !StringUtils.equals(getMyDomainName(), extractedDomain)) {
+        if (StringUtils.isNotEmpty(extractedDomain) && !StringUtils.equalsIgnoreCase(getMyDomainName(), extractedDomain)) {
             UserStoreManager secondaryUserStoreManager = getSecondaryUserStoreManager(extractedDomain);
             if (secondaryUserStoreManager != null) {
                 userManager = secondaryUserStoreManager;
@@ -3022,7 +3024,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
                             + " for the given domain name.");
                 }
             } else {
-                throw new UserStoreClientException("Invalid Domain Name.");
+                throw new UserStoreClientException("Invalid Domain Name: " + extractedDomain);
             }
         }
 
@@ -9577,7 +9579,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
         }
 
         UserStoreManager userManager = this;
-        if (StringUtils.isNotEmpty(extractedDomain)) {
+        if (StringUtils.isNotEmpty(extractedDomain) && !StringUtils.equalsIgnoreCase(getMyDomainName(), extractedDomain)) {
             UserStoreManager secondaryUserStoreManager = getSecondaryUserStoreManager(extractedDomain);
             if (secondaryUserStoreManager != null) {
                 userManager = secondaryUserStoreManager;
@@ -9585,6 +9587,8 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
                     log.debug("Domain: " + extractedDomain + " is passed with the claim and user store manager is loaded"
                             + " for the given domain name.");
                 }
+            } else {
+                throw new UserStoreClientException("Invalid Domain Name: " + extractedDomain);
             }
         }
 
@@ -14657,8 +14661,11 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
         }
 
         UserStoreManager userManager = this;
-        if (StringUtils.isNotEmpty(extractedDomain)) {
+        if (StringUtils.isNotEmpty(extractedDomain) && !StringUtils.equalsIgnoreCase(getMyDomainName(), extractedDomain)) {
             userManager = getSecondaryUserStoreManager(extractedDomain);
+            if (userManager == null) {
+                throw new UserStoreClientException("Invalid Domain Name: " + extractedDomain);
+            }
             if (log.isDebugEnabled()) {
                 log.debug("Domain: " + extractedDomain + " is passed with the claim and user store manager is loaded"
                         + " for the given domain name.");
