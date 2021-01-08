cf7e24b3a5fec44f82b968b94b9bb8d79618857c
dewniMW <dewni.matheesha@gmail.com>
2021-01-08 07:47:25 +0530

    Trim attribute values obtained from user store.

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
@@ -11834,8 +11834,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
                     property = mapping.getMappedAttribute();
                 }
 
-                value = userPropertyValues.get(property);
-                if (value != null && value.trim().length() > 0) {
+                if (StringUtils.isNotBlank(value = userPropertyValues.get(property))) {
                     finalValues.put(claim, value);
                 }
 
@@ -11844,8 +11843,7 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
                     property = this.realmConfig.getUserStoreProperty(LDAPConstants.DISPLAY_NAME_ATTRIBUTE);
                 }
 
-                value = userPropertyValues.get(property);
-                if (value != null && value.trim().length() > 0) {
+                if (StringUtils.isNotBlank(value = userPropertyValues.get(property))) {
                     finalValues.put(claim, value);
                 }
             }
@@ -15044,9 +15042,11 @@ public abstract class AbstractUserStoreManager implements PaginatedUserStoreMana
 
             for (String claim : claims) {
                 for (Map.Entry<String, String> userAttribute : entry.getValue().entrySet()) {
+                    String value;
                     if (claimToAttributeMap.get(claim) != null && claimToAttributeMap.get(claim)
-                            .equals(userAttribute.getKey())) {
-                        userClaims.put(claim, userAttribute.getValue());
+                            .equals(userAttribute.getKey()) && StringUtils.isNotBlank(value = userAttribute.getValue())
+                    ) {
+                        userClaims.put(claim, value);
                     }
                 }
             }
