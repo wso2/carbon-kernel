417efaac3e203eb4f0c666da749a0220d5b03115
sachiniWettasinghe <swettasinghe23@gmail.com>
2021-02-17 23:38:43 +0530

    Use equal operator to filter users based on an attribute

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/util/JDBCRealmUtil.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/util/JDBCRealmUtil.java
@@ -195,13 +195,25 @@ public class JDBCRealmUtil {
             properties.put(JDBCRealmConstants.GET_USERS_FOR_PROP,
                     JDBCRealmConstants.GET_USERS_FOR_PROP_SQL);
         }
+        if (!properties.containsKey(JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE)) {
+            properties.put(JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE,
+                    JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE_SQL);
+        }
         if (!properties.containsKey(JDBCRealmConstants.GET_USERS_FOR_PROP_WITH_ID)) {
             properties.put(JDBCRealmConstants.GET_USERS_FOR_PROP_WITH_ID,
                     JDBCRealmConstants.GET_USERS_FOR_PROP_WITH_ID_SQL);
         }
+        if (!properties.containsKey(JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID)) {
+            properties.put(JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID,
+                    JDBCRealmConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID_SQL);
+        }
         if (!properties.containsKey(JDBCCaseInsensitiveConstants.GET_USERS_FOR_PROP_WITH_ID_CASE_INSENSITIVE)) {
             properties.put(JDBCCaseInsensitiveConstants.GET_USERS_FOR_PROP_WITH_ID_CASE_INSENSITIVE,
                     JDBCCaseInsensitiveConstants.GET_USERS_FOR_PROP_WITH_ID_SQL_CASE_INSENSITIVE);
+        }
+        if (!properties.containsKey(JDBCCaseInsensitiveConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID_CASE_INSENSITIVE)) {
+            properties.put(JDBCCaseInsensitiveConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID_CASE_INSENSITIVE,
+                    JDBCCaseInsensitiveConstants.GET_USERS_FOR_CLAIM_VALUE_WITH_ID_SQL_CASE_INSENSITIVE);
         }
         if (!properties.containsKey(JDBCRealmConstants.GET_PAGINATED_USERS_FOR_PROP)) {
             properties.put(JDBCRealmConstants.GET_PAGINATED_USERS_FOR_PROP,
