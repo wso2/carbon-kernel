772219a06bcb60d50b8f1dcdd1f9ec7a0cf05a45
AnuradhaSK <anuradha199528@gmail.com>
2021-08-19 15:18:35 +0530

    fix formatting issue and client component resolution

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/CarbonConstants.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/CarbonConstants.java
@@ -35,8 +35,9 @@ public final class CarbonConstants {
     public static final String AUDIT_MESSAGE = "Initiator : %s | Action : %s | Target : %s | Data : { %s } | Result : %s ";
     public static final String DISABLE_LEGACY_AUDIT_LOGS = "disableLegacyAuditLogs";
     public static final String AUDIT_LOG_MESSAGE_TEMPLATE =
-            "{id : %s, recordedAt : %s, clientComponent : %s, correlationId : %s, initiator.id : %s, initiator.name : %s, initiator.type : %s, event.type : %s, target.id : %s, target.name : %s, target.type : %s, data.change : { %s }}";
-
+            "{id : %s, recordedAt : %s, clientComponent : %s, correlationId : %s, " +
+                    "initiator.id : %s, initiator.name : %s, initiator.type : %s, event.type : %s, target.id : %s, " +
+                    "target.name : %s, target.type : %s, data.change : %s }";
     /**
      * This is used to get root context within CarbonJNDIContext when we need to operate
      * with LDAP.
