ed39575da5cd8bb201d1ca58f34661ad792112f4
Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>
2021-09-13 15:48:39 +0530

    Fix a possible NPE when overlapping delete and get users. (#3070)
    * Fix a possible NPE when overlapping delete and get users.
    
    * Add null check.
    
    * Address comments

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/util/UserCoreUtil.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/util/UserCoreUtil.java
@@ -582,6 +582,9 @@ public final class UserCoreUtil {
      */
     public static String addDomainToName(String name, String domainName) {
 
+        if (StringUtils.isEmpty(name)) {
+            return null;
+        }
         if ((name.indexOf(UserCoreConstants.DOMAIN_SEPARATOR)) < 0 &&
                 !UserCoreConstants.PRIMARY_DEFAULT_DOMAIN_NAME.equalsIgnoreCase(domainName)) {
             // domain name is not already appended, and if exist in user-mgt.xml, append it..
