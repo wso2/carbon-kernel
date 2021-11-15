135428ffb724620a96bb0049a3a837bacd938ae3
ChanikaRuchini <chanikaruchini.16@cse.mrt.ac.lk>
2021-11-15 14:31:15 +0530

    Fix Identity claim filtering

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCUserStoreManager.java
@@ -4519,13 +4519,13 @@ public class JDBCUserStoreManager extends AbstractUserStoreManager {
         sqlBuilder.updateSqlWithOROperation("UA.UM_ATTR_NAME = ?", attributeName);
 
         if (ExpressionOperation.EQ.toString().equals(operation)) {
-            sqlBuilder.updateSqlWithOROperation("UA.UM_ATTR_VALUE = ?", attributeValue);
+            sqlBuilder.where("UA.UM_ATTR_VALUE = ?", attributeValue);
         } else if (ExpressionOperation.EW.toString().equals(operation)) {
-            sqlBuilder.updateSqlWithOROperation("UA.UM_ATTR_VALUE LIKE ?", "%" + attributeValue);
+            sqlBuilder.where("UA.UM_ATTR_VALUE LIKE ?", "%" + attributeValue);
         } else if (ExpressionOperation.CO.toString().equals(operation)) {
-            sqlBuilder.updateSqlWithOROperation("UA.UM_ATTR_VALUE LIKE ?", "%" + attributeValue + "%");
+            sqlBuilder.where("UA.UM_ATTR_VALUE LIKE ?", "%" + attributeValue + "%");
         } else if (ExpressionOperation.SW.toString().equals(operation)) {
-            sqlBuilder.updateSqlWithOROperation("UA.UM_ATTR_VALUE LIKE ?", attributeValue + "%");
+            sqlBuilder.where("UA.UM_ATTR_VALUE LIKE ?", attributeValue + "%");
         }
     }
 
@@ -4549,7 +4549,12 @@ public class JDBCUserStoreManager extends AbstractUserStoreManager {
     private void getExpressionConditions(Condition condition, List<ExpressionCondition> expressionConditions) {
 
         if (condition instanceof ExpressionCondition) {
-            expressionConditions.add((ExpressionCondition) condition);
+            ExpressionCondition expressionCondition = (ExpressionCondition) condition;
+            if (StringUtils.isNotEmpty(expressionCondition.getAttributeName()) ||
+                    StringUtils.isNotEmpty(expressionCondition.getAttributeValue()) ||
+                    StringUtils.isNotEmpty(expressionCondition.getOperation())) {
+                expressionConditions.add(expressionCondition);
+            }
         } else if (condition instanceof OperationalCondition) {
             Condition leftCondition = ((OperationalCondition) condition).getLeftCondition();
             getExpressionConditions(leftCondition, expressionConditions);
