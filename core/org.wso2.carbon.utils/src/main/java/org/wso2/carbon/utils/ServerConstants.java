eb0caf094e1982295c2e2fd24511c76385e96e5a
Anuradha Karunarathna <anuradha199528@gmail.com>
2022-03-22 15:04:21 +0530

    introduce a system property given high priority

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/ServerConstants.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/ServerConstants.java
@@ -63,10 +63,14 @@ public final class ServerConstants {
     public static final String SERVICE_TYPE = "serviceType";
 	
     /**
-     * System property to disable/ enable server shutdown or restart remotely using APIs.
+     * Config to disable/ enable server shutdown or restart remotely using APIs.
      */
     public static final String ENABLE_REMOTE_RESTART = "enableShutdownRestartFromAPI.enableRestart";
     public static final String ENABLE_REMOTE_SHUTDOWN = "enableShutdownRestartFromAPI.enableShutdown";
+    /**
+     * System property to disable/ enable server shutdown or restart remotely using APIs.
+     */
+    public static final String ENABLE_REMOTE_SHUTDOWN_AND_RESTART = "enableRemoteShutdownAndRestart";
 
     /**
      * This is the key of the System property which indicates whether the server is running
