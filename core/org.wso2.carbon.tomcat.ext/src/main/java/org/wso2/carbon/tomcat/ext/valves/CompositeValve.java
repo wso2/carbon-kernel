191ce1f176b37f817de4b909a1baccfbd62006c0
shanggeeth kanagalingam <shanggeethk@gmail.com>
2022-08-24 11:40:46 +0530

    Fix formatting issues

--- core/org.wso2.carbon.tomcat.ext/src/main/java/org/wso2/carbon/tomcat/ext/valves/CompositeValve.java
+++ core/org.wso2.carbon.tomcat.ext/src/main/java/org/wso2/carbon/tomcat/ext/valves/CompositeValve.java
@@ -43,9 +43,9 @@ public class CompositeValve extends ValveBase {
         try {
             if (request.getContext() == null) {
                 if (request.getRequestURI() == null) {
-             if (log.isDebugEnabled()) {
-                    log.debug("Could not handle the request. The request URI is invalid.");
-             }
+                    if (log.isDebugEnabled()) {
+                        log.debug("Could not handle the request. The request URI is invalid.");
+                    }
                     return;
                 }
                 if (log.isDebugEnabled()) {
