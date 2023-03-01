46b6ab9318d363f177e83ca7136867b1b1eee2be
Chamath Samarawickrama <chamaths@wso2.com>
2023-03-01 14:30:21 +0530

    Upgrade p2 plugin version and carbon-parent version

--- core/javax.cache/src/main/java/org/wso2/carbon/caching/impl/CarbonCacheManager.java
+++ core/javax.cache/src/main/java/org/wso2/carbon/caching/impl/CarbonCacheManager.java
@@ -203,14 +203,6 @@ public class CarbonCacheManager implements CacheManager {
     }
 
     @Override
-    public javax.transaction.UserTransaction getUserTransaction() {
-        Util.checkAccess(ownerTenantDomain, ownerTenantId);
-        checkStatusStarted();
-        touch();
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
     public boolean isSupported(OptionalFeature optionalFeature) {
         Util.checkAccess(ownerTenantDomain, ownerTenantId);
         checkStatusStarted();
