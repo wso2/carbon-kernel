2d903fbe96d008d15f96d080288429cad3951472
LakshikaAthapaththu <lakshikaa@wso2.com>
2023-07-17 11:21:48 +0530

    Address comments

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/CustomHostNameVerifier.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/CustomHostNameVerifier.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.utils;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.http.conn.ssl.AbstractVerifier;
-
 import javax.net.ssl.SSLException;
+import java.util.Optional;
 
 /**
  * Custom hostname verifier class.
@@ -13,16 +30,17 @@ public class CustomHostNameVerifier extends AbstractVerifier {
     private final static String[] LOCALHOSTS = {"::1", "127.0.0.1", "localhost", "localhost.localdomain"};
 
     @Override
-    public void verify(String s, String[] strings, String[] subjectAlts) throws SSLException {
+    public void verify(String hostname, String[] commonNames, String[] subjectAlternativeNames) throws SSLException {
 
-        String[] subjectAltsWithLocalhosts = ArrayUtils.addAll(subjectAlts, LOCALHOSTS);
+        String[] subjectAltsWithLocalhosts = ArrayUtils.addAll(subjectAlternativeNames, LOCALHOSTS);
 
-        if (strings != null && strings.length > 0 && strings[0] != null) {
-
-            String[] subjectAltsWithLocalhostsAndCN = ArrayUtils.add(subjectAltsWithLocalhosts, strings[0]);
-            this.verify(s, strings, subjectAltsWithLocalhostsAndCN, false);
-        } else {
-            this.verify(s, strings, subjectAltsWithLocalhosts, false);
+        boolean isValidCommonNames = Optional.ofNullable(commonNames)
+                .filter(names -> names.length > 0)
+                .map(names -> names[0])
+                .isPresent();
+        if (isValidCommonNames && !ArrayUtils.contains(subjectAlternativeNames, commonNames[0])) {
+            subjectAltsWithLocalhosts = ArrayUtils.add(subjectAltsWithLocalhosts, commonNames[0]);
         }
+        this.verify(hostname, commonNames, subjectAltsWithLocalhosts, false);
     }
 }
--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/HTTPClientUtils.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/HTTPClientUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.utils;
 
 import org.apache.http.conn.ssl.X509HostnameVerifier;
@@ -20,7 +37,7 @@ public class HTTPClientUtils {
      *
      * @return HttpClientBuilder.
      */
-    public static HttpClientBuilder getHTTPClientWithCustomHostNameVerifier() {
+    public static HttpClientBuilder createClientWithCustomVerifier() {
 
         HttpClientBuilder httpClientBuilder = HttpClientBuilder.create().useSystemProperties();
         if (DEFAULT_AND_LOCALHOST.equals(System.getProperty(HOST_NAME_VERIFIER))) {
