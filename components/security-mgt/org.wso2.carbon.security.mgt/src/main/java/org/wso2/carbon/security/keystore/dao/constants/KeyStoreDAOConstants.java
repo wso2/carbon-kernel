2d4abd68bec7f738de4603a932790fcb79ed31d1
mpmadhavig <madhavi.17@cse.mrt.ac.lk>
2023-08-15 23:53:58 +0530

    Remove tenant ID from get key store by id query.

--- components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/dao/constants/KeyStoreDAOConstants.java
+++ components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/dao/constants/KeyStoreDAOConstants.java
@@ -23,11 +23,16 @@ package org.wso2.carbon.security.keystore.dao.constants;
  */
 public class KeyStoreDAOConstants {
 
-    private KeyStoreDAOConstants() {}
+    private KeyStoreDAOConstants() {
+
+    }
 
     public static class KeyStoreTableColumns {
 
-        private KeyStoreTableColumns() {}
+        private KeyStoreTableColumns() {
+
+        }
+
         public static final String ID = "ID";
         public static final String FILE_NAME = "FILE_NAME";
         public static final String TYPE = "TYPE";
@@ -43,24 +48,23 @@ public class KeyStoreDAOConstants {
 
     public static class SqlQueries {
 
-        private SqlQueries() {}
+        private SqlQueries() {
+
+        }
 
         public static final String ADD_KEY_STORE = "INSERT INTO IDN_KEY_STORE " +
-            "(ID, FILE_NAME, TYPE, PROVIDER, PASSWORD, PRIVATE_KEY_ALIAS, PRIVATE_KEY_PASS, TENANT_UUID, LAST_UPDATED, CONTENT) " +
-            "VALUES (:ID;, :FILE_NAME;, :TYPE;, :PROVIDER;, :PASSWORD;, :PRIVATE_KEY_ALIAS;, :PRIVATE_KEY_PASS;, :TENANT_UUID;, :LAST_UPDATED;, ?)";
+                "(ID, FILE_NAME, TYPE, PROVIDER, PASSWORD, PRIVATE_KEY_ALIAS, PRIVATE_KEY_PASS, TENANT_UUID, " +
+                "LAST_UPDATED, CONTENT) VALUES (:ID;, :FILE_NAME;, :TYPE;, :PROVIDER;, :PASSWORD;, " +
+                ":PRIVATE_KEY_ALIAS;, :PRIVATE_KEY_PASS;, :TENANT_UUID;, :LAST_UPDATED;, ?)";
 
         public static final String UPDATE_KEY_STORE_BY_FILE_NAME = "UPDATE IDN_KEY_STORE SET TYPE = :TYPE;, " +
                 "PROVIDER = :PROVIDER;, PASSWORD = :PASSWORD;, PRIVATE_KEY_ALIAS = :PRIVATE_KEY_ALIAS;, " +
                 "PRIVATE_KEY_PASS = :PRIVATE_KEY_PASS;, LAST_UPDATED = :LAST_UPDATED;, CONTENT = ? " +
                 "WHERE FILE_NAME = :FILE_NAME; AND TENANT_UUID = :TENANT_UUID;";
 
-
-
-        // TODO: do we really need to qualify this with tenant id?
         public static final String GET_KEY_STORE_BY_ID =
-                "SELECT * FROM IDN_KEY_STORE WHERE ID = :ID; AND TENANT_UUID = :TENANT_UUID;";
+                "SELECT * FROM IDN_KEY_STORE WHERE ID = :ID;";
 
-        // TODO: add unique constraint to file name and tenant id
         public static final String GET_KEY_STORE_BY_FILE_NAME =
                 "SELECT * FROM IDN_KEY_STORE WHERE FILE_NAME = :FILE_NAME; AND TENANT_UUID = :TENANT_UUID;";
 
@@ -79,7 +83,7 @@ public class KeyStoreDAOConstants {
                 "SELECT PUB_CERT_ID FROM IDN_KEY_STORE WHERE FILE_NAME = :FILE_NAME; AND TENANT_UUID = :TENANT_UUID;";
 
         public static final String ADD_PUB_CERT_ID_TO_KEY_STORE =
-                "UPDATE IDN_KEY_STORE SET PUB_CERT_ID = :PUB_CERT_ID;, LAST_UPDATED = :LAST_UPDATED; WHERE FILE_NAME = :FILE_NAME; AND TENANT_UUID = :TENANT_UUID;";
+                "UPDATE IDN_KEY_STORE SET PUB_CERT_ID = :PUB_CERT_ID;, LAST_UPDATED = :LAST_UPDATED; " +
+                        "WHERE FILE_NAME = :FILE_NAME; AND TENANT_UUID = :TENANT_UUID;";
     }
-
 }
