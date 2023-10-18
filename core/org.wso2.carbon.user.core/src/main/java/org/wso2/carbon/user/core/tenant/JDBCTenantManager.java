2da36ea72845e5cba0220875a4cf2bf83cca3ae9
prathabanKavin <kavinprathaban025@gmail.com>
2023-10-18 16:01:54 +0530

    Fix database type check for MSSQL

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/JDBCTenantManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/JDBCTenantManager.java
@@ -1165,7 +1165,7 @@ public class JDBCTenantManager implements TenantManager {
             prepStmt = dbConnection.prepareStatement(sqlQuery);
             prepStmt.setInt(1, offset + limit);
             prepStmt.setInt(2, offset);
-        } else if (dbType.equalsIgnoreCase("Microsoft")) {
+        } else if (dbType.equalsIgnoreCase("Microsoft SQL Server")) {
             sqlTail = String.format(TenantConstants.LIST_TENANTS_MSSQL_TAIL, sortedOrder);
             sqlQuery = sqlQuery + sqlTail;
             prepStmt = dbConnection.prepareStatement(sqlQuery);
