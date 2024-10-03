106813713e0eff5ed3dba72476df9bb583ccdc33
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-10-03 12:05:37 +0530

    Update CarbonSecurityManager.java

--- core/org.wso2.carbon.bootstrap/src/main/java/org/wso2/carbon/bootstrap/CarbonSecurityManager.java
+++ core/org.wso2.carbon.bootstrap/src/main/java/org/wso2/carbon/bootstrap/CarbonSecurityManager.java
@@ -1,24 +1,22 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.carbon.bootstrap;
-
-import sun.security.util.SecurityConstants;
 
 import java.security.AccessControlContext;
 import java.security.AccessControlException;
@@ -33,6 +31,9 @@ import java.util.List;
 public class CarbonSecurityManager extends SecurityManager {
 
     private List<String> deniedProperties = new ArrayList<String>();
+    // String replacement of sun.security.util.SecurityConstants.MODIFY_THREADGROUP_PERMISSION
+    private static final RuntimePermission MODIFY_THREADGROUP_PERMISSION =
+            new RuntimePermission("modifyThreadGroup");
 
     public CarbonSecurityManager() {
         super();
@@ -86,7 +87,7 @@ public class CarbonSecurityManager extends SecurityManager {
             throw new NullPointerException("thread group can't be null");
         }
 
-        checkPermission(SecurityConstants.MODIFY_THREADGROUP_PERMISSION);
+        checkPermission(MODIFY_THREADGROUP_PERMISSION);
     }
 
     /**
@@ -100,7 +101,7 @@ public class CarbonSecurityManager extends SecurityManager {
             throw new NullPointerException("thread can't be null");
         }
 
-        checkPermission(SecurityConstants.MODIFY_THREADGROUP_PERMISSION);
+        checkPermission(MODIFY_THREADGROUP_PERMISSION);
     }
 
 
