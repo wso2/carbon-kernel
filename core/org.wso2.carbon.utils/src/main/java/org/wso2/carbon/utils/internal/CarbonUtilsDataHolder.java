612f66fe7911d2a333d9bc226f7c07898f97afde
Darshana Gunawardana <darshana@wso2.com>
2024-12-20 11:00:47 +0530

    Lazy load keystore datasource

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/internal/CarbonUtilsDataHolder.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/internal/CarbonUtilsDataHolder.java
@@ -1,14 +1,20 @@
 package org.wso2.carbon.utils.internal;
 
 import org.apache.axis2.context.ConfigurationContext;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.utils.CarbonUtils;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class CarbonUtilsDataHolder {
 
+    private static final Log LOG = LogFactory.getLog(CarbonUtilsDataHolder.class);
     private static ConfigurationContext configContext;
-    private DataSource dataSource;
+    private static DataSource dataSource;
 
     private static CarbonUtilsDataHolder carbonUtilsDataHolder = new CarbonUtilsDataHolder();
 
@@ -28,11 +34,33 @@ public class CarbonUtilsDataHolder {
 
     public DataSource getDataSource() {
 
-        return this.dataSource;
+        if (dataSource == null) {
+            initializeDatasource();
+        }
+
+        return dataSource;
     }
 
     public void setDataSource(DataSource dataSource) {
 
         this.dataSource = dataSource;
+    }
+
+    private static synchronized DataSource initializeDatasource() {
+
+        String dataSourceName = CarbonUtils.getServerConfiguration().getFirstProperty(
+                "KeyStoreDataPersistenceManager.DataSourceName");
+        if (StringUtils.isNotBlank(dataSourceName)) {
+            try {
+                dataSource = InitialContext.doLookup(dataSourceName);
+                if (dataSource == null) {
+                    LOG.warn("Data source not found: " + dataSourceName);
+                }
+            } catch (NamingException e) {
+                throw new RuntimeException("Error in looking up keystore data source.", e);
+            }
+        } else {
+            throw new RuntimeException("Data source name is not configured for KeyStore Data Persistence Manager.");
+        }
     }
 }
