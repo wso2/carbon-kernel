a8c3e87a130df15cfb182d61eb72efb6f83df410
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2025-06-03 18:48:21 +0530

    Add hybrid queries

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCUserStoreConstants.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/jdbc/JDBCUserStoreConstants.java
@@ -56,7 +56,6 @@ public class JDBCUserStoreConstants {
             "display name of the user";
     public static final String DISPLAY_NAME_ATTRIBUTE = "DisplayNameAttribute";
     public static final String STORE_USER_ATTRIBUTE_VALUE_AS_UNICODE = "StoreUserAttributeValueAsUnicode";
-    public static final String USER_ATTRIBUTE_MIGRATION_STATE = "UserAttributeColumnMigrationState";
 
     static {
 
@@ -658,7 +657,7 @@ public class JDBCUserStoreConstants {
                 "On Delete User, Remove User Attribute SQL With Case Insensitive Username",
                 JDBCCaseInsensitiveConstants.ON_DELETE_USER_REMOVE_ATTRIBUTE_SQL_CASE_INSENSITIVE, "",
                 new Property[] { USER.getProperty(), SQL.getProperty(), FALSE.getProperty() });
-        setAdvancedProperty(USER_ATTRIBUTE_MIGRATION_STATE, "User Attribute Migration State", "",
+        setAdvancedProperty(JDBCRealmConstants.USER_ATTRIBUTE_MIGRATION_STATE, "User Attribute Migration State", "",
                 "Defines the current state of the user attribute column migration. This controls whether " +
                         "user attributes are read from or written to the old or new (Unicode-supported) column. " +
                         "If unset, the default legacy behavior is used.",
