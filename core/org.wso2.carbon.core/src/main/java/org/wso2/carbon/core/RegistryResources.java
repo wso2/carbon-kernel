e004ad0fd95a3bc839669d1280934c84c456608f
Rivindu <rivindum.15@cse.mrt.ac.lk>
2026-02-12 16:18:22 +0530

    Add HSM support for primary keystore

--- core/org.wso2.carbon.core/src/main/java/org/wso2/carbon/core/RegistryResources.java
+++ core/org.wso2.carbon.core/src/main/java/org/wso2/carbon/core/RegistryResources.java
@@ -145,6 +145,12 @@ public final class RegistryResources {
         public static final String SERVER_PRIVATE_KEY_PASSWORD = "Security.KeyStore.KeyPassword";
         public static final String SERVER_PRIMARY_KEYSTORE_TYPE = "Security.KeyStore.Type";
 
+        //HSM key store
+        public static final String SERVER_HSM_KEYSTORE_ENABLED = "Security.HSMKeyStore.Enabled";
+        public static final String SERVER_HSM_KEYSTORE_PROVIDER_CONFIG_FILE = "Security.HSMKeyStore.ProviderConfiguration";
+        public static final String SERVER_HSM_KEYSTORE_PASSWORD = "Security.HSMKeyStore.Password";
+        public static final String SERVER_HSM_KEYSTORE_KEY_ALIAS = "Security.HSMKeyStore.KeyAlias";
+
         public static final String DEFAULT_SECURITY_CERTIFICATE_ALIAS = "wso2carbon";
 
         //Registry store
